29350e4fe0802d0ffb9e9ec03d3e28baf36c5db0
mohmmedalkhatim <developmentaccun@outlook.com>
2024-12-11 19:48:04 +0200

    start rebuilding the commands file structure

--- src-tauri/src/app/commands/mod.rs
+++ src-tauri/src/app/commands/mod.rs
@@ -1,6 +1,6 @@
 
 
-pub mod area_controller;
+pub mod area;
 pub mod archive_controller;
 pub mod project_controller;
 pub mod resources_controller;
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,13 +1,18 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-
+use app::database_connection;
+use async_std::sync::Mutex;
+use sea_orm::DatabaseConnection;
 
 mod app;
 
+struct DbConnection {
+    db: Mutex<DatabaseConnection>,
+}
 
-fn main() {
-    
+#[tokio::main]
+async fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![
@@ -15,7 +20,10 @@ fn main() {
             app::archive_control,
             app::projects_control,
             app::resources_control,
-            ])
+        ])
+        .manage(DbConnection {
+            db: Mutex::from(database_connection().await),
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
