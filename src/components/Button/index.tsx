71e2f7e56460e35f200a683ba8b9e44b22737f78
mohmmedalkhatim <developmentaccun@outlook.com>
2025-01-11 07:51:49 +0200

    adding the buttom componenet

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,12 @@
+function Button ({ content }: { content: String }) {
+  return (
+    <button
+      data-ripple-light='true'
+      type='button'
+      className='select-none rounded-md bg-sky-500 py-2 px-3 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none'
+    >
+      {content}
+    </button>
+  )
+}
+export default Button
