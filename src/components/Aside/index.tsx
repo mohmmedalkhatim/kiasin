8ec8e7122f5a727ffe62eebbbd2af235540bbc53
mohmmedalkhatim <developmentaccun@outlook.com>
2025-01-13 10:25:47 +0200

    working on the aside element

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,6 +1,12 @@
-function Aside() {
-  return (
-    <aside >Aside</aside>
-  )
+import './style.css'
+import Links from './Links'
+import Profile_link from './profile'
+
+
+function Aside () {
+  return <aside className='aside'>
+    <Profile_link/>
+    <Links/>
+  </aside>
 }
 export default Aside
