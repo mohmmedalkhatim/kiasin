754579a232349798c877ac0556f243467e49a003
mohmmedalkhatim <developmentaccun@outlook.com>
2025-02-07 18:24:41 +0200

    working on the useLayout function

--- src/context/page_schema.ts
+++ src/context/page_schema.ts
@@ -1,13 +1,31 @@
+import { create, createStore } from 'zustand'
 interface Card {
   id: string
   props: any
   content: string
+  width: number
+  height: number
+}
+type setType = {
+  (
+    partial:
+      | Layout
+      | Partial<Layout>
+      | ((state: Layout) => Layout | Partial<Layout>),
+    replace?: false
+  ): void
+  (state: Layout | ((state: Layout) => Layout), replace: true): void
+}
+class Layout {
+  public list?: Card[]
+  public sort_list?: string[]
+  constructor (set: setType, schema: string) {
+    this.list = JSON.parse(schema)
+    this.sort_list = this.list?.map(item => item.id)
+  }
+  updatalists (Cardlist: string[]) {
+    this.sort_list = Cardlist
+  }
 }
 
-class Cards_control{
-    list?:Card[]
-    getIdsList():Array<String>{
-        return this.list?.map(item=>item.id) || []
-    }
-
-}
+let useLayout = create<Layout>()
