87837940deb65b6b1d4ad04a9aa87c87cee361bc
mohmmedalkhatim <developmentaccun@outlook.com>
2025-02-18 18:56:13 +0200

    deleting som unused commponet

--- src/components/board/board_components/note/index.tsx
+++
@@ -1,7 +0,0 @@
-function Note({id}:{id:"string"}) {
-  return (
-    <div className="grow rounded-md m_border col-start-auto p-3  row-start-auto col-span-3 row-span-4 bg-[#181818]">
-    </div>
-  )
-}
-export default Note
