2ba430ecd5b3e1b299afa3cc2d31276b7a128d24
mohmmedalkhatim <developmentaccun@outlook.com>
2025-02-21 15:27:34 +0200

    working on para context

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,20 @@
-import gsap from "gsap"
-import { ReactLenis } from 'lenis/react'
-import { useEffect, useRef } from 'react'
+
 import { Outlet } from 'react-router-dom'
 import Aside from './components/Aside'
 import Header from './components/Header'
 import './App.css'
+import { usePara } from './context/para'
+import { useEffect } from 'react'
+
 function App() {
-  const lenisRef = useRef<any>(null)
-
-  useEffect(() => {
-    function update(time: number) {
-      lenisRef.current?.lenis?.raf(time * 1000)
-    }
-
-    gsap.ticker.add(update)
-
-    return () => gsap.ticker.remove(update)
-  }, [])
-
+  let init = usePara((state)=>state.init) 
+  useEffect(()=>{
+    init()
+  },[])
   return (
     <>
       <Header />
-      <ReactLenis options={{ autoRaf: false }} ref={lenisRef}>
-        <Outlet />
-      </ReactLenis>
+      <Outlet />
       <Aside />
     </>
   )
---
+++ src/context/para/index.tsx
@@ -0,0 +1,31 @@
+import { create } from "zustand";
+import { listen } from '@tauri-apps/api/event';
+import { Area } from "../../types/area";
+import { Project } from "../../types/project";
+import { Resource } from "../../types/resource";
+
+interface Para {
+    projects: Project[]
+    areas: Area[],
+    resources: Resource[],
+    init: () => void
+}
+export let usePara = create<Para>((set) => ({
+    projects: [],
+    areas: [],
+    resources: [],
+    init: async () => {
+        listen<Area[]>("areas", e => {
+            set({ areas: e.payload })
+        })
+        listen<Project[]>("projects", e => {
+            set({ projects: e.payload })
+        })
+        listen<Resource[]>("resources", e => {
+            set({ resources: e.payload })
+        })
+    }
+    
+}))
+
+
