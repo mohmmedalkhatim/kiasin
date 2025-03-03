2e725ab809e5b807b6dc5f59c3500943510ec64a
mohmmedalkhatim <developmentaccun@outlook.com>
2025-03-03 09:56:59 +0200

    fixing a bug

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,16 @@ import { Outlet } from 'react-router-dom'
 import Aside from './components/Aside'
 import Header from './components/Header'
 import './App.css'
-import { usePara } from './context/para'
 import { useEffect } from 'react'
 import { useAuth } from './context/User'
 import Login from './screens/login'
 
 function App() {
-  let init = usePara((state) => state.init);
   let email = useAuth((state) =>state.email);
   let auth = useAuth((state) =>state.auth);
   let content = email == '' ? <Login /> : <><Header /><Outlet /><Aside /></>
   useEffect(() => {
     auth()
-    init()
   }, [])
   return (
     <>
