56ff73377527e95fe341db93c710c41e15a3455d
mohmmedalkhatim <developmentaccun@outlook.com>
2025-03-09 19:17:47 +0200

    refactor toolbar to use BubbleMenu; integrate alignment and heading components for improved text formatting

--- src/components/Editor/toolbar/index.tsx
+++ src/components/Editor/toolbar/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
-import { Editor } from "@tiptap/react";
-import { IconAlignCenter, IconAlignJustified, IconAlignLeft, IconAlignRight, IconBold, IconH1, IconH2, IconH3, IconH4, IconH5, IconH6, IconItalic, IconUnderline } from "@tabler/icons-react"
+import { BubbleMenu, Editor } from "@tiptap/react";
+import { IconAlignCenter, IconAlignJustified, IconAlignLeft, IconAlignRight, IconBold, IconH1, IconH2, IconH3, IconH4, IconH5, IconH6, IconHeading, IconItalic, IconUnderline } from "@tabler/icons-react"
+import Heading from "./heading";
+import Alignment from "./alignment";
 
 interface ToolbarProps {
   editor: Editor | null;
@@ -11,91 +13,28 @@ export const Toolbar: React.FC<ToolbarProps> = ({ editor }) => {
   if (!editor) return null;
 
   return (
-    <div className="toolbar">
-      <button
-        onClick={() => editor.chain().focus().toggleBold().run()}
-        disabled={!editor.can().chain().focus().toggleBold().run()}
-        className="toolbar_button"
-      >
-        <IconBold />
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleItalic().run()}
-        disabled={!editor.can().chain().focus().toggleItalic().run()}
-        className="toolbar_button"
-      >
-        <IconItalic />
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
-        disabled={!editor.can().chain().focus().toggleHeading({ level: 1 }).run()}
-        className="toolbar_button"
-      >
-        <IconH1 />
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
-        disabled={!editor.can().chain().focus().toggleHeading({ level: 2 }).run()}
-        className="toolbar_button"
-      >
-        <IconH2 />
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
-        disabled={!editor.can().chain().focus().toggleHeading({ level: 3 }).run()}
-        className="toolbar_button"
-      >
-        <IconH3 />
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 4 }).run()}
-        disabled={!editor.can().chain().focus().toggleHeading({ level: 4 }).run()}
-        className="toolbar_button"
-      >
-        <IconH4 />
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 5 }).run()}
-        disabled={!editor.can().chain().focus().toggleHeading({ level: 5 }).run()}
-        className="toolbar_button"
-      >
-        <IconH5 />
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 6 }).run()}
-        disabled={!editor.can().chain().focus().toggleHeading({ level: 6 }).run()}
-        className="toolbar_button"
-      >
-        <IconH6 />
-      </button>
-      <button
-        onClick={() => editor.chain().focus().setTextAlign("center").run()}
-        disabled={!editor.can().chain().focus().setTextAlign("center").run()}
-        className="toolbar_button"
-      >
-        <IconAlignCenter />
-      </button>
-      <button
-        onClick={() => editor.chain().focus().setTextAlign("right").run()}
-        disabled={!editor.can().chain().focus().setTextAlign("right").run()}
-        className="toolbar_button"
-      >
-        <IconAlignRight />
-      </button>
-      <button
-        onClick={() => editor.chain().focus().setTextAlign("left").run()}
-        disabled={!editor.can().chain().focus().setTextAlign("left").run()}
-        className="toolbar_button"
-      >
-        <IconAlignLeft />
-      </button>
-      <button
-        onClick={() => editor.chain().focus().setTextAlign("justify").run()}
-        disabled={!editor.can().chain().focus().setTextAlign("justify").run()}
-        className="toolbar_button"
-      >
-        <IconAlignJustified />
-      </button>
+    <BubbleMenu editor={editor} className="bubble_menu">
+      <div className="flex">
+        <button
+          onClick={() => editor.chain().focus().toggleBold().run()}
+          disabled={!editor.can().chain().focus().toggleBold().run()}
+          className="toolbar_button"
+        >
+          <IconBold />
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleItalic().run()}
+          disabled={!editor.can().chain().focus().toggleItalic().run()}
+          className="toolbar_button"
+        >
+          <IconItalic />
+        </button>
+
+        <div className="flex">
+          <Alignment editor={editor}/>
+          <Heading editor={editor}/>
+        </div>
+      </div>
       <button
         onClick={() => editor.chain().focus().setUnderline().run()}
         disabled={!editor.can().chain().focus().setUnderline().run()}
@@ -103,7 +42,7 @@ export const Toolbar: React.FC<ToolbarProps> = ({ editor }) => {
       >
         <IconUnderline />
       </button>
-      {/* Add more buttons for other formatting options */}
-    </div>
+    </BubbleMenu>
   );
+
 };
