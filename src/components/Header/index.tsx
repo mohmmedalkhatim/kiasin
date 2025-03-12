7c30c65fa1f1d0fc7bf4e114240ee176f22e8127
mohmmedalkhatim <developmentaccun@outlook.com>
2025-03-12 11:52:04 +0200

    add button styles and implement edit functionality in Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,17 @@
 import { useState } from "react"
 import Input from "../Input"
+import { IconEdit } from "@tabler/icons-react"
+import { useAreas } from "../../context/para/areas"
 
 function Header() {
   let [search, setSearch] = useState()
+  let taggle = useAreas(state=>state.taggleEditable)
   return (
     <header className="app_header">
       <div></div>
       <Input type={"text"} props={search} action={(e) => { setSearch(e.target.value) }} />
       <div></div>
-      <div></div>
+      <button onClick={taggle}>edit <span><IconEdit size={"1rem"} /></span></button>
       <div></div>
     </header>
   )
