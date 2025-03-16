29266ea577177e1a9dd8084a8cc1193f0a9f4111
mohmmedalkhatim <developmentaccun@outlook.com>
2025-03-16 15:35:18 +0200

    feat: implement draggable TaskList component with basic styling

--- src/components/board/components/Editor/index.tsx
+++ src/components/board/components/Editor/index.tsx
@@ -1,27 +1,11 @@
 import { useEditor, EditorContent} from '@tiptap/react';
 import { } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
-import { Underline } from '@tiptap/extension-underline';
 import TextAlign from '@tiptap/extension-text-align';
-import textStyle from '@tiptap/extension-text-style';
 import './style.css';
 
 
 const Editor_card = ({content,title}:{content:string,title:string}) => {
-    const titleEditor = useEditor({
-        extensions: [
-            StarterKit,
-            TextAlign.configure({
-                types: ["heading"],
-            }),
-        ],
-        content:`<h4>${title}<h4/>`,
-        editorProps: {
-            attributes: {
-                class: 'editor',
-            },
-        }
-    });
     const editor = useEditor({
         extensions: [
             StarterKit,
@@ -39,8 +23,7 @@ const Editor_card = ({content,title}:{content:string,title:string}) => {
 
     return (
         <div className='eidtor_container'>
-            <EditorContent cols={1} className='editor' editor={titleEditor} />
-            <EditorContent cols={10} className='editor' editor={editor} />
+            <EditorContent className='editor' editor={editor} />
         </div>
     )
 };
---
+++ src/components/board/components/Tasklist/Task/index.tsx
@@ -0,0 +1,6 @@
+function Task({id,classn}:{id:number,classn:string}) {
+  return (
+    <div className={`${classn} Task`}>Task</div>
+  )
+}
+export default Task
