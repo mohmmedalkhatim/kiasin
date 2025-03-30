77bf7050125f04821c9862f6ea21d6f05cc90e63
mohmmedalkhatim <developmentaccun@outlook.com>
2025-03-30 21:12:09 +0200

    refactor Aside component layout and add toggle functionality

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,14 +1,20 @@
 import Cards_List from "./cards_list"
 import Templates_List from "./templates"
 import "./index.css"
+import { useAside } from "../../context/aside";
+import { IconChevronsRight } from "@tabler/icons-react";
 
 
 
 function Aside({ active, T }: { active: boolean, T: string }) {
     let content = T == "area" ? <Templates_List /> : <Cards_List />;
+    let toggle = useAside(state => state.toggle);
     return (
         <aside className={`${active && "hide"} Aside`}>
-            {content}
+            <IconChevronsRight className="toggle" onClick={toggle}/>
+                <div className="content_container">
+                    {content}
+                </div>
         </aside>
     )
 }
