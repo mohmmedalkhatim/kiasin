73acb74814be91bd7680763abcb8d976c38bf2bc
mohmmedalkhatim <developmentaccun@outlook.com>
2025-04-06 17:04:46 +0200

    calculate the position of the element

--- src/components/bubble_menu/index.tsx
+++ src/components/bubble_menu/index.tsx
@@ -1,14 +1,26 @@
-import { useRef, useState } from "react";
+import React, { EventHandler, useRef, useState } from "react";
 import { IconPlus } from "@tabler/icons-react";
+
+const calculate_menu_appernce = (y: number): number => {
+    let pos = 0;
+    if (y <= 300) {
+        pos = 72
+    } else {
+        pos = 260
+    }
+    return pos
+}
 
 function Cards_menu({ handleadding }: { handleadding: (s: string) => void }) {
     const [menu, setmenu] = useState(false)
     const menuRef = useRef<HTMLDivElement>(null)
+
     let change = (e: any) => {
+
         if (menuRef.current) {
-            menuRef.current.style.left = `${e.pageX - 20}px`;
-            menuRef.current.style.top = `${e.pageY - 12}px`;
-            console.log("x :" + e.movementX + "y :" + e.movementY)
+            menuRef.current.style.left = `${e.pageX - 260}px`;
+            menuRef.current.style.top = `${e.pageY - calculate_menu_appernce(e.pageY)}px`;
+            console.log(e.pageY)
         }
         setmenu(!menu)
     }
@@ -19,7 +31,9 @@ function Cards_menu({ handleadding }: { handleadding: (s: string) => void }) {
                     <IconPlus size={20} />
                 </button>
             </div>
-            <div ref={menuRef} className={`${menu ? "menu" : "hide"}`}>hello</div>
+            <div ref={menuRef} className={`${menu ? "menu" : "hide"} z-1000`}>
+
+            </div>
         </>
     )
 }
