fb3fa68534b5e1b54e7bee1005dc5b6025cca686
mohmmedalkhatim <developmentaccun@outlook.com>
2025-04-14 17:37:53 +0200

    fix a bug in the dashborad

--- src/screens/dashborad/index.tsx
+++ src/screens/dashborad/index.tsx
@@ -3,10 +3,10 @@ import Layout from '../../Layout';
 import { invoke } from '@tauri-apps/api/core';
 import { Area } from '../../types/area';
 import { useQuery } from '@tanstack/react-query';
-import { useActionData } from 'react-router-dom';
 import { useAreas } from '../../context/para/areas';
 
 function DashBoard({}: {}) {
+  let setActive = useAreas(state=>state.update_active_area)
   const { data, error, isLoading } = useQuery({
     queryKey: ['area'],
     queryFn: async () => await invoke<Area>('dashboard'),
@@ -14,7 +14,6 @@ function DashBoard({}: {}) {
   if (isLoading) return <main className="content">Loading...</main>;
   if (error) return <main className="content">Error: {error.message}</main>;
   if (data) {
-    let setActive = useAreas(state=>state.update_active_area)
     setActive(data)
     return (
       <main className="content">
