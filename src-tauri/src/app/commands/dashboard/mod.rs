a823f7ab7f39463fe7710a46030b66f5ad3b9b50
mohmmedalkhatim <developmentaccun@outlook.com>
2025-04-24 09:03:13 +0200

    changing the dashboard picture

--- src-tauri/src/app/commands/dashboard/mod.rs
+++ src-tauri/src/app/commands/dashboard/mod.rs
@@ -10,7 +10,7 @@ pub async fn dashboard() -> Area {
                 {"id":0,"cols":6,"rows":4,"type":"Areaslist","props":{"list":[1,2,3,4]}},
                 {"id":1,"cols":2,"rows":8,"type":"tasks","props":{"list":[1,2,3,4,5,6]}},
                 {"id":2,"cols":3,"rows":4,"type":"editor","props":"hello world"},
-                {"id":3,"cols":2,"rows":4,"type":"image","props":"/engineer.jpeg"},
+                {"id":3,"cols":2,"rows":4,"type":"image","props":"/ignitelight.jpeg"},
                 {"id":4,"cols":8,"rows":4,"type":"calender","props":""},
             ]
         }),
