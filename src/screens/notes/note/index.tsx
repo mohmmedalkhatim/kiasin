655ee725f943bf5da9585b4259eb6ddb7148a037
mohmmedalkhatim <developmentaccun@outlook.com>
2025-04-25 07:57:46 +0200

    adding global style to and implementing the editor in the note page to add more functionalities

--- src/screens/notes/note/index.tsx
+++ src/screens/notes/note/index.tsx
@@ -1,22 +1,50 @@
 import { useParams } from 'react-router-dom';
-import Editor from '../../../components/Editor';
+import Editor from './Editor';
 import { useNotes } from '../../../context/para/notes';
 import { useEffect, useState } from 'react';
 import { Note } from '../../../types/notes';
+import { useEditor } from '@tiptap/react';
+import Placeholder from '@tiptap/extension-placeholder';
+import TextAlign from '@tiptap/extension-text-align';
+import Underline from '@tiptap/extension-underline';
+import Highlight from '@tiptap/extension-highlight';
+import textStyle from '@tiptap/extension-text-style';
+import StarterKit from '@tiptap/starter-kit';
 
 function NotePage() {
   const get = useNotes((state) => state.note);
   const [notes, set_notes] = useState<Note>();
   const { id } = useParams();
   const activeList = useNotes((state) => state.active);
+  let content = notes?.content;
   useEffect(() => {
     get(Number(id));
     set_notes(activeList.at(-1));
   }, []);
-  return (
-    <div className="content">
-      <Editor content={notes?.content as string} />
-    </div>
-  );
+  const editor = useEditor({
+    extensions: [
+      StarterKit,
+      Underline,
+      TextAlign.configure({
+        types: ['heading', 'paragraph'],
+      }),
+      Highlight.configure(),
+      textStyle,
+      Placeholder.configure({ placeholder: 'start writing ...' }),
+    ],
+    content,
+    editorProps: {
+      attributes: {
+        class: 'editor',
+      },
+    },
+  });
+  if (editor) {
+    return (
+      <div className="content">
+        <Editor editor={editor} />
+      </div>
+    );
+  }
 }
 export default NotePage;
