c249537e1d7ae325bec5a85f8f26d1974086e48d
mohmmedalkhatim <developmentaccun@outlook.com>
2025-04-25 08:02:20 +0200

    moving files and implementing  HeaderBar components for more functionalities in the editor

---
+++ src/screens/notes/note/headersbar/index.tsx
@@ -0,0 +1,8 @@
+import { Editor } from "@tiptap/react"
+
+function HeaderBar({editor}:{editor:Editor}) {
+  return (
+    <nav className="HeaderBar">HeaderBar</nav>
+  )
+}
+export default HeaderBar
