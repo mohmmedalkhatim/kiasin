7606dd2525d6ddff8f34d48154c430e994b5d074
mohmmedalkhatim <developmentaccun@outlook.com>
2025-05-03 13:59:34 +0200

    working on the style of the note component

--- src/components/Cards/note_card/index.tsx
+++ src/components/Cards/note_card/index.tsx
@@ -8,10 +8,11 @@ const NoteCard = ({ note }: { note: Note }) => {
       className="relative flex flex-col rounded-md m_border bg-clip-border text-gray-700 shadow-md"
     >
       <div className="p-4">
-        <h5 className="block text-md font-sans font-semibold leading-snug tracking-normal text-white antialiased">
+        <h5 className="block text-md mb-3 font-sans font-semibold leading-snug tracking-normal text-white antialiased">
           {note.title}
         </h5>
-        <div className="px-4 py-3 flex items-center text-xs flex justify-between">
+        <div  className='m_border'/>
+        <div className="px-2 py-3 flex items-center text-xs flex justify-between">
           <p>{note.description}</p>
         </div>
       </div>
