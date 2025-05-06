05039e32b684c655037ba0ee0da69d1937f418bf
mohmmedalkhatim <developmentaccun@outlook.com>
2025-05-06 21:24:49 +0200

    starting the logic of the useTasks hook

---
+++ src/context/para/tasks.tsx
@@ -0,0 +1,32 @@
+import { Channel, invoke } from "@tauri-apps/api/core"
+import { create } from "zustand"
+import { Todo } from "../../types/todos"
+
+
+interface Tasks {
+    list: Todo[]
+    init: () => void
+    create: (name: string) => void
+    get_list: (ids: string[]) => void
+    
+}
+
+let useTasks = create<Tasks>(set => ({
+    list: [],
+    init: () => {
+        let channel = new Channel<Todo[]>();
+        channel.onmessage = (list) => {
+            set({ list })
+        }
+        invoke("todos_control", { payload: { command: "find" } })
+    },
+    create: (name: string) => {
+        let channel = new Channel<Todo[]>();
+        channel.onmessage = (data) => {
+            set(state => ({ list: [...state.list, data[0]] }))
+        }
+        invoke("todos_control", { payload: { command: "create", name } })
+    },
+    get_list: (ids) => { }
+
+}))
