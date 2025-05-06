cdf9eeab6376b8f8b5cc3f2bf409ac8be6cc1fb8
mohmmedalkhatim <developmentaccun@outlook.com>
2025-05-06 21:24:09 +0200

    adding new component for the navbar element and

---
+++ src/components/Navbar/Links/link/index.tsx
@@ -0,0 +1,20 @@
+import { ReactElement } from "react"
+import { Link } from "react-router-dom"
+
+
+interface props {
+    id: number,
+    link: string,
+    Icon: ReactElement,
+    name: string
+}
+
+function Page(item: props) {
+    return (
+        <Link key={item.id} className="link group rounded z-40" to={item.link}>
+            <div >{item.Icon}</div>
+            <div className="hidden m_border group-hover:block tip">{item.name}</div>
+        </Link>
+    )
+}
+export default Page
