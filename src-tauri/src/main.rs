a25a1ff9481b0d09b537024d88252eb379bddafd
mohmmedalkhatim <developmentaccun@outlook.com>
2025-05-14 14:48:55 +0200

    adding the logic for single instance plugin

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -16,7 +16,18 @@ struct DbConnection {
 
 #[tokio::main]
 async fn main() {
-    tauri::Builder::default()
+    let mut builder = tauri::Builder::default();
+    #[cfg(desktop)]
+    {
+        builder = builder.plugin(tauri_plugin_single_instance::init(|app, args, cwd| {
+            let _ = app
+                .get_webview_window("main")
+                .expect("no main window")
+                .set_focus();
+        }));
+    }
+
+    builder
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![
