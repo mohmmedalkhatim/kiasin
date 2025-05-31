f557f1325fee89bde45ff7547f316ced4d1a75ef
mohmmedalkhatim <developmentaccun@outlook.com>
2025-05-31 06:53:49 +0200

    adding more way to retrieve information about media entity

--- src-tauri/src/app/commands/media/functions/retrive.rs
+++ src-tauri/src/app/commands/media/functions/retrive.rs
@@ -1,6 +1,17 @@
 use migration::entities::media::{Entity, Model};
-use sea_orm::{DatabaseConnection, EntityTrait};
+use sea_orm::{DatabaseConnection, DbErr, EntityTrait};
 
-pub async fn find_many(db: &DatabaseConnection) -> Result<Vec<Model>, String> {
+pub async fn find_many(db: &DatabaseConnection) -> Result<Vec<Model>, DbErr> {
     Ok(Entity::find().all(db).await.unwrap())
+}
+pub async fn find_list(ids: Vec<i32>, db: &DatabaseConnection) -> Result<Vec<Model>, DbErr> {
+    let mut list = Vec::new();
+    for id in ids {
+        let item = Entity::find_by_id(id).one(db).await?.unwrap();
+        list.push(item);
+    }
+    Ok(list)
+}
+pub async fn find_one(id: i32, db: &DatabaseConnection) -> Result<Model, DbErr> {
+    Ok(Entity::find_by_id(id).one(db).await?.unwrap())
 }
