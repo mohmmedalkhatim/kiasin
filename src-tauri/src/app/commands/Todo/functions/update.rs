551f4337d2befc4d50671b877b42907ab18f4e35
mohmmedalkhatim <developmentaccun@outlook.com>
2025-06-01 09:09:35 +0200

    improve the logic to update the task

---
+++ src-tauri/src/app/commands/Todo/functions/update.rs
@@ -0,0 +1,18 @@
+use migration::entities::todo::{ActiveModel, Entity};
+use sea_orm::{entity::*, DatabaseConnection, DbErr, EntityTrait, Set};
+
+use crate::app::commands::todo::objects::Todo;
+
+pub async fn update_note(todo: Todo, db: &DatabaseConnection) -> Result<(), DbErr> {
+    let time = chrono::Local::now();
+    let new = ActiveModel {
+        id: Set(todo.id.unwrap()),
+        title: Set(todo.title),
+        area_id: Set(todo.area_id),
+        checked: Set(todo.checked),
+        update:Set(time.date_naive()),
+        ..Default::default()
+    };
+    let _ = Entity::update(new).exec(db).await?;
+    Ok(())
+}
