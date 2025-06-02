40157145d4055f33cfb5161c790e0a1df98ec906
mohmmedalkhatim <developmentaccun@outlook.com>
2025-06-02 15:48:48 +0200

    optimizing useMedia hook by adding more logic and separating the sended object for the media object

--- src/context/para/media.tsx
+++ src/context/para/media.tsx
@@ -1,6 +1,17 @@
 import { Channel, invoke } from '@tauri-apps/api/core';
 import { create } from 'zustand';
+import { useAreas } from './areas';
+import React from 'react';
 
+type send_Media = {
+  id?: number;
+  file: string;
+  url?: string;
+  note_id?: number;
+  project_id?: number;
+  area_id?: number;
+  media_type: string;
+};
 type Media = {
   id?: number;
   file: number[];
@@ -12,30 +23,35 @@ type Media = {
 };
 
 interface media {
-  list: Media[];
-  create: (data:Media) => string;
+  create: (
+    data: send_Media,
+    store_fun: (media: Media[]) => void
+  ) => Promise<void>;
+  handleFileChange: (
+    event: React.ChangeEvent<HTMLInputElement>,
+    id: number,
+
+  ) => void;
   update: () => string;
   delete: () => void;
-  one: (id: number) => string;
+  get_image_url: (
+    id: number,
+    set_url: React.Dispatch<React.SetStateAction<string>>,
+  ) => void;
   get_list: (ids: number[]) => string[];
 }
 
-export let useMedia = create<media>(set => ({
-  list: [],
-  create: (data) => {
-    let channel = new Channel<Media>();
-    let url: string[] = [];
-    channel.onmessage = media => {
-      let file = new Blob([new Uint8Array(media.file)], {
-        type: media.media_type,
-      });
-      url.push(URL.createObjectURL(file));
-    };
-    invoke("media_control",{payload:{
-        command:"create",
-        item:data,
-    }})
-    return url[0];
+export let useMedia = create<media>(_ => ({
+  create: async (data, store_fun) => {
+    let channel = new Channel<Media[]>();
+    channel.onmessage = store_fun;
+    await invoke('media_control', {
+      payload: {
+        command: 'create',
+        item: data,
+      },
+      channel,
+    });
   },
   update: () => {
     let channel = new Channel<Media>();
@@ -49,17 +65,18 @@ export let useMedia = create<media>(set => ({
     return url[0];
   },
   delete: () => {},
-  one: id => {
-    let channel = new Channel<Media>();
-    let url: string[] = [];
+  get_image_url: (id, set_url) => {
+    let channel = new Channel<Media[]>();
     channel.onmessage = media => {
-      let file = new Blob([new Uint8Array(media.file)], {
-        type: media.media_type,
+      let file = new Blob([new Uint8Array(media[0].file)], {
+        type: media[0].media_type,
       });
-      url.push(URL.createObjectURL(file));
-      file = new Blob();
+      console.log(media[0].file);
+      let url = URL.createObjectURL(file);
+      console.log(file);
+      set_url(url);
     };
-    return '';
+    invoke('media_control', { payload: { command: 'find', id }, channel });
   },
   get_list: () => {
     let channel = new Channel<Media[]>();
@@ -77,5 +94,36 @@ export let useMedia = create<media>(set => ({
       });
     };
     return url;
+  },
+  handleFileChange: (
+    event: React.ChangeEvent<HTMLInputElement>,
+    id: number,
+  ) => {
+    const files = event.target.files;
+    if (!files || files.length === 0) return;
+    const file = files[0];
+    const reader = new FileReader();
+    reader.onload = () => {
+      let [info, file] = reader.result?.toString().split(',') as string[];
+      let create = useMedia.getState().create;
+      let store: (m: Media[]) => void = media => {
+        let card = useAreas.getState().get_Card(id);
+        card.props = media[0].id;
+        useAreas.getState().update_card(id, card);
+      };
+      create(
+        {
+          file,
+          media_type: info,
+        },
+        store
+      );
+    };
+
+    reader.onerror = e => {
+      console.error('Error reading file:', e);
+    };
+
+    reader.readAsDataURL(file);
   },
 }));
