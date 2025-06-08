c75d08750a1905de086dab59badf3a0953488aeb
mohmmedalkhatim <developmentaccun@outlook.com>
2025-06-08 12:30:15 +0200

    fixing a dedounce concept to fix a rendering issus

--- src/Layout/components/List/index.tsx
+++ src/Layout/components/List/index.tsx
@@ -2,26 +2,35 @@ import { IconLayersIntersect } from '@tabler/icons-react';
 import Card from '../../../components/Cards/area_card';
 import { useAreas } from '../../../context/para/areas';
 import { useState } from 'react';
+import { useDebounce } from 'react-use';
 
-function AreasList({ id }: { id: number }) {
-  let update_card = useAreas((state) => state.update_card);
-  let get_card = useAreas((state) => state.get_Card);
-  let card = get_card(id).props;
-  let [areas] = useState<number[]>(card.list);
+function AreasList ({ id }: { id: number }) {
+  let update_card = useAreas(state => state.update_card);
+  let list = useAreas(state => state.list);
+  let get_card = useAreas(state => state.get_Card);
+  let [areas, setAreas] = useState<number[]>([]);
+  useDebounce(
+    () => {
+      let card = get_card(id).props;
+      setAreas(card.list);
+    },
+    20,
+    [list]
+  );
   return (
-    <div className="p-4">
-      <div className="px-4 pb-2 flex items-center justify-between">
-        <div className="">
+    <div className='p-4'>
+      <div className='px-4 pb-2 flex items-center justify-between'>
+        <div className=''>
           <div>areas</div>
-          <div className="border"></div>
+          <div className='border'></div>
         </div>
         <div onClick={() => {}}>
           <IconLayersIntersect />
         </div>
       </div>
-      <div className="m_border  mb-3 "></div>
-      <div className="boxs_grid">
-        {areas.map((item) => (
+      <div className='m_border  mb-3 '></div>
+      <div className='boxs_grid'>
+        {areas.map(item => (
           <Card key={item} id={item} />
         ))}
       </div>
