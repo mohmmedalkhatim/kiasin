6843cf3ebb2a57e31e20c2ff9969a76b781bdecf
mohmmedalkhatim <developmentaccun@outlook.com>
2025-06-09 06:43:28 +0200

    adding the update functionality and fixing the debounce delay

--- src/Layout/components/Tasklist/Task/index.tsx
+++ src/Layout/components/Tasklist/Task/index.tsx
@@ -5,38 +5,54 @@ import { useTasks } from '../../../../context/para/tasks';
 import { Todo } from '../../../../types/todos';
 import { useState } from 'react';
 import { useDebounce } from 'react-use';
-import {  IconGridDots } from '@tabler/icons-react';
+import { IconGridDots } from '@tabler/icons-react';
+import { useLayoutDialog } from '../../../../context/para/Dialog';
+import { useAreas } from '../../../../context/para/areas';
+import { useNotes } from '../../../../context/para/notes';
 
-function Task ({ id, classname }: { id: number; classname: string }) {
+function Task ({
+  id,
+  classname,
+  link,
+}: {
+  id: number;
+  classname: string;
+  link?: number;
+}) {
   const one = useTasks(state => state.get_one);
   const [task, setTask] = useState({} as Todo);
   const [checked, setChecked] = useState(task.checked || false);
-  let update = useTasks(state=>state.update)
-  const {
-    attributes,
-    listeners,
-    setNodeRef,
-    transform,
-    transition,
-  } = useSortable({ id });
+  const dialog = useLayoutDialog(state => state.changeMode);
+  const get_card = useAreas(state=>state.get_Card)
+  const update = useTasks(state => state.update);
+  const { attributes, listeners, setNodeRef, transform, transition } =
+    useSortable({ id });
   const style = {
     transform: CSS.Transform.toString(transform),
     transition,
   };
   useDebounce(
     () => {
-      one(String(id), setTask,setChecked).then(() => {});
+      one(String(id), setTask, setChecked).then(() => {});
     },
     200,
     []
   );
-  useDebounce(()=>{
-    update({
-      id: task.id,
-      title: task.title,
-      checked,
-    },setTask,setChecked)
-  },300,[checked])
+  useDebounce(
+    () => {
+      update(
+        {
+          id: task.id,
+          title: task.title,
+          checked,
+        },
+        setTask,
+        setChecked
+      );
+    },
+    10,
+    [checked]
+  );
 
   return (
     <div
@@ -46,8 +62,17 @@ function Task ({ id, classname }: { id: number; classname: string }) {
       {...attributes}
     >
       <div className='text-sm flex gap-4'>
-        <IconGridDots {...listeners} className='cursor-grab'  size={'1rem'}  />
-        <div className=' cursor-pointer'>{task.title}</div>
+        <IconGridDots {...listeners} className='cursor-grab' size={'1rem'} />
+        <div
+          className='cursor-pointer'
+          onClick={() => {
+            if (!link) {
+              dialog('dialog_note', { id: task.note_id as number });
+            }else{}
+          }}
+        >
+          {task.title}
+        </div>
       </div>
       <Checkbox state={checked} setState={setChecked} />
     </div>
