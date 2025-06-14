d649879065aa78e966505d20053cc7827151d6f9
mohmmedalkhatim <developmentaccun@outlook.com>
2025-06-14 18:27:15 +0200

    fixing bugs on the dialog

--- src/Layout/Dialog/index.tsx
+++ src/Layout/Dialog/index.tsx
@@ -1,16 +1,20 @@
+import { IconX } from '@tabler/icons-react';
 import { useLayoutDialog } from '../../context/para/Dialog';
 import Content from './content';
-import DialogHeader from './Header';
 import './style.css';
 
 function Dialog () {
-  let state = useLayoutDialog(state => state.state);
-  let mode = useLayoutDialog(state => state.mode);
+  const props = useLayoutDialog(state => state.props);
+  const state = useLayoutDialog(state => state.state);
+  const mode = useLayoutDialog(state => state.mode);
+  const toggle = useLayoutDialog(state => state.toggle);
   if (state) {
     return (
       <div className={mode + ' dialog m_border'}>
-        <DialogHeader />
-        <Content mode={mode} props={state}/>
+        <div className='absolute top-5 right-5' onClick={toggle}>
+          <IconX />
+        </div>
+        <Content mode={mode} props={props} />
       </div>
     );
   }
