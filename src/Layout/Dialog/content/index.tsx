69c199829e1e80a9ddc9d872d0c7d3adc84fdb3e
mohmmedalkhatim <developmentaccun@outlook.com>
2025-06-14 18:39:44 +0200

    adding an option to the dialog content component

--- src/Layout/Dialog/content/index.tsx
+++ src/Layout/Dialog/content/index.tsx
@@ -1,12 +1,14 @@
 import { useState } from 'react';
 import Editor from '../../components/Editor';
 import Cards from './Cards';
+import Areas from './Area/Areas';
 
 function Content ({ props, mode }: { props: any; mode: string }) {
   let [element, setElement] = useState(
     new Map([
       ['dialog_note', <Editor id={props.id} content={''} title={''} />],
-      ['dialog_links', <Cards />],
+      ['dialog_links', <Cards id={props.id} />],
+      ['dialog_areas', <Areas id={props.id} />],
       ['default', <div>no Content</div>],
     ])
   );
