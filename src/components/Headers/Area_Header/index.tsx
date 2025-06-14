1cb1930564801057326fd3c8701066de3e03722d
mohmmedalkhatim <developmentaccun@outlook.com>
2025-06-14 18:26:17 +0200

    rebuild the header component to link to a note page

--- src/components/Headers/Area_Header/index.tsx
+++ src/components/Headers/Area_Header/index.tsx
@@ -1,26 +1,27 @@
 import { useState } from 'react';
 import Input from '../../Input';
-import { IconEdit, IconSearch } from '@tabler/icons-react';
+import { IconEdit, IconSearch, IconNotebook } from '@tabler/icons-react';
 import { useAreas } from '../../../context/para/areas';
 import Button from '../../Button';
+import { Link } from 'react-router-dom';
 
-function Header() {
+function Header () {
   const [search, setSearch] = useState('');
-  const toggle = useAreas((state) => state.toggleEditable);
-  const active = useAreas(state=>state.active)
+  const toggle = useAreas(state => state.toggleEditable);
+  const active = useAreas(state => state.active)?.at(-1);
   return (
-    <header className="app_header">
+    <header className='app_header'>
       <div></div>
       <Input
         type={'text'}
         value={search}
-        onChange={()=>setSearch}
+        onChange={() => setSearch}
         icon={<IconSearch size={'1rem'} />}
       />
-      <div>{active?.at(-1)?.title}</div>
+      <div>{active?.title}</div>
       <Button
-        size="sm"
-        className="bg-[#181818]"
+        size='sm'
+        className='bg-[#181818]'
         children={
           <>
             edit <IconEdit size={'1.2rem'} />
@@ -28,6 +29,9 @@ function Header() {
         }
         onClick={toggle}
       />
+      <Link to={`/note/${active?.note_id}`}>
+        <IconNotebook size={'2rem'} />
+      </Link>
       <div></div>
     </header>
   );
