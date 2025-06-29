fbf9ec496515c759314978bc087c35819f60e7c9
mohmmedalkhatim <developmentaccun@outlook.com>
2025-06-29 18:04:44 +0200

    fixing typo issus and fix new types adding logic to create a note issus

--- src-tauri/src/app/commands/Todo/functions.rs
+++ src-tauri/src/app/commands/Todo/functions.rs
@@ -3,7 +3,7 @@ mod delete;
 mod retrive;
 mod update;
 
-pub use create::create_note;
+pub use create::create_todo;
 pub use delete::delete_one;
 pub use retrive::{find_all, find_list, find_one};
-pub use update::update_note;
+pub use update::update_todo;
--- src-tauri/src/app/commands/areas/functions/create.rs
+++ src-tauri/src/app/commands/areas/functions/create.rs
@@ -1,6 +1,5 @@
-use core::time;
 
-use chrono::{Date, Local};
+use chrono::Local;
 use migration::entities::{
     area::{ActiveModel, Entity},
     note, template,
@@ -8,12 +7,12 @@ use migration::entities::{
 use sea_orm::{DatabaseConnection, DbErr, EntityTrait, IntoActiveModel, Set};
 use serde_json::json;
 
+
 pub async fn create_area(db: &DatabaseConnection, id: i32) -> Result<i32, DbErr> {
     let shema = json!({
         "item":[]
     });
-    let mut date = Local::now();
-    let model = template::Entity::find_by_id(id as u32).one(db).await;
+    let date = Local::now();
     let active_note = note::ActiveModel {
         title: Set(Some("untitled".to_string())),
         content: Set(Some(json!({"content":""}))),
@@ -22,29 +21,42 @@ pub async fn create_area(db: &DatabaseConnection, id: i32) -> Result<i32, DbErr>
         ..Default::default()
     };
     let note = note::Entity::insert(active_note).exec(db).await?;
-
+    let note_id = note.last_insert_id;
     let links = json!({ "list": [] });
     let new = ActiveModel {
         title: Set(Some("untitled".to_string())),
         user_id: Set(1),
-        descrption: Set(Some("set a discrption".to_string())),
+        descrption: Set(Some("set a description".to_string())),
         icon: Set(None),
         cover: Set(None),
         links: Set(links),
         created: Set(Some(date.date_naive())),
         ui_schema: Set(shema),
-        note_id:Set(note.last_insert_id),
+        note_id:Set(note_id.clone()),
         in_archive: Set(false),
         categorie: Set(id as u32),
         ..Default::default()
     };
-
-    Ok(Entity::insert(new).exec(db).await?.last_insert_id as i32)
+    let id = Entity::insert(new).exec(db).await?.last_insert_id as i32;
+   let _ = update_note_area_id(note_id, id, db);
+    Ok(id)
 }
+
+
+pub async fn update_note_area_id(note_id:i32 , id: i32, db: &DatabaseConnection) -> Result<(), DbErr> {
+    let new = note::ActiveModel {
+        id: Set(note_id),
+        area_id:Set(Some(id)),
+        ..Default::default()
+    };
+    let _ = note::Entity::update(new).exec(db).await?;
+    Ok(())
+}
+
 
 pub async fn create_from_templates(db: &DatabaseConnection, id: i32) -> Result<i32, DbErr> {
     let links = json!({ "list": [] });
-    let mut date = Local::now();
+    let date = Local::now();
     let model = template::Entity::find_by_id(id as u32).one(db).await;
     let active_note = note::ActiveModel {
         title: Set(Some("untitled".to_string())),
