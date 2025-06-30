4fe67e655880679b77904f3b8182a38d4a4afdd2
mohmmedalkhatim <developmentaccun@outlook.com>
2025-06-30 09:05:15 +0200

    completing the navbar functionality

--- src/Layout/Navbar/index.tsx
+++ src/Layout/Navbar/index.tsx
@@ -1,6 +1,56 @@
-function Navbar() {
-  return (
-    <nav>Navbar</nav>
-  )
+import {
+  DndContext,
+  closestCenter,
+  DragOverlay,
+  useSensor,
+  KeyboardSensor,
+  PointerSensor,
+  useSensors,
+} from '@dnd-kit/core';
+import {
+  SortableContext,
+  verticalListSortingStrategy,
+} from '@dnd-kit/sortable';
+import CardSort from './CardSort';
+import Cards_menu from '../bubble_menu';
+import { element_props } from '..';
+
+interface Navbar_props {
+  start: (event: any) => void;
+  end: (event: any) => void;
+  state: boolean;
+  schema: string[];
+  create: (element: element_props) => void;
 }
-export default Navbar
+
+function Navbar ({ start, end, state, schema, create }: Navbar_props) {
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor)
+  );
+
+  return (
+    <nav
+      className='fixed w-[16rem] transition-all duration-150 h-screen z-60 pt-20 px-4  bg-[#181818] m_border'
+      style={{ top: 0, right: state ? '0' : '9999px' }}
+    >
+      <Cards_menu handleadding={create} />
+      <DndContext
+        sensors={sensors}
+        collisionDetection={closestCenter}
+        onDragStart={start}
+        onDragEnd={end}
+      >
+        <SortableContext items={schema} strategy={verticalListSortingStrategy}>
+          <div className='flex flex-col gap-2  select-none'>
+            {schema.map(item => (
+              <CardSort id={item} />
+            ))}
+          </div>
+        </SortableContext>
+        <DragOverlay></DragOverlay>
+      </DndContext>
+    </nav>
+  );
+}
+export default Navbar;
