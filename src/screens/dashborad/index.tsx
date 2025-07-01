9df095095876d6f5af7adaeb2a48e51b2a529aed
mohmmedalkhatim <developmentaccun@outlook.com>
2025-07-01 12:11:21 +0200

    adding new functionality to the mix

--- src/screens/dashborad/index.tsx
+++ src/screens/dashborad/index.tsx
@@ -5,21 +5,29 @@ import { Area } from '../../types/area';
 import { useQuery } from '@tanstack/react-query';
 import { useAreas } from '../../context/para/areas';
 import Header from '../../components/Headers/Area_Header';
+import { Store } from 'tauri-plugin-store-api';
 
-function DashBoard({}: {}) {
-  let setActive = useAreas((state) => state.update_active_area);
+function DashBoard ({}: {}) {
+  const setActive = useAreas(state => state.update_active_area);
+  const editable = useAreas(state=>state.editable);
+
+  const dashboard_store = new Store('./dashboard');
+
   const { data, error, isLoading } = useQuery({
     queryKey: ['area'],
-    queryFn: async () => await invoke<Area>('dashboard'),
+    queryFn: async () => invoke<Area>("dashboard"),
   });
-  if (isLoading) return <main className="content">Loading...</main>;
-  if (error) return <main className="content">Error: {error.message}</main>;
+  if (isLoading) return <main className='content'>Loading...</main>;
+  if (error) {
+    console.log(error)
+    return <main className='content'>Error: {error.message}</main>;
+  }
   if (data) {
     setActive(data);
     return (
       <>
         <Header />
-        <main className="content">
+        <main className={!editable?"content":"mt-[5rem] mr-[16rem]"}>
           <Layout />
         </main>
       </>
