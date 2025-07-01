8d10e18462614dd38b783312a3733f14815dd6b9
mohmmedalkhatim <developmentaccun@outlook.com>
2025-07-01 12:13:10 +0200

    working on responsiveness and adding new property to the Todo

--- src/util/index.ts
+++ src/util/index.ts
@@ -5,8 +5,8 @@ export function classNames(
 }
 export function get_card_cols (col: number, container_width: number): number {
   if (container_width == 0) return col;
-  if (container_width < 400) return 1;
-  if (container_width < 800 && col > 2) return 2;
+  if (container_width < 540) return 1;
+  if (container_width < 980 && col > 2) return 2;
   if (container_width < 1100 && col > 3) return 3;
   return col;
 }
