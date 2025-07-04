d9bd924a743f8c750daca48632a16f7e592e116b
mohmmedalkhatim <developmentaccun@outlook.com>
2025-07-04 06:58:26 +0200

    adding the logic to change the area name

--- src/components/Headers/Area_Header/index.tsx
+++ src/components/Headers/Area_Header/index.tsx
@@ -1,14 +1,21 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Input from '../../Input';
 import { IconEdit, IconSearch, IconNotebook } from '@tabler/icons-react';
 import { useAreas } from '../../../context/para/areas';
 import Button from '../../Button';
 import { Link } from 'react-router-dom';
+import { Area } from '../../../types/area';
 
 function Header () {
   const [search, setSearch] = useState('');
   const toggle = useAreas(state => state.toggleEditable);
   const active = useAreas(state => state.active)?.at(-1);
+  let [state, setState] = useState(1);
+  const update = useAreas(state => state.update);
+  const update_active = useAreas(state => state.update_active_area);
+  useEffect(() => {
+    setState(state + 1);
+  }, [active]);
   return (
     <header className='app_header'>
       <div></div>
@@ -18,7 +25,15 @@ function Header () {
         onChange={() => setSearch}
         icon={<IconSearch size={'1rem'} />}
       />
-      <div>{active?.title}</div>
+      <input
+        value={active?.title}
+        className='outline-none border-none focus:border'
+        onChange={e => {
+          let area = { ...active, title: e.target.value } as Area;
+          update_active(area);
+          update(area);
+        }}
+      />
       <Button
         size='sm'
         className='bg-[#181818]'
