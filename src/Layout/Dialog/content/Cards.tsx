c8ab0d3ba45c183ddfcc4ab903d52ef77dd507c4
mohmmedalkhatim <developmentaccun@outlook.com>
2025-07-08 15:35:29 +0200

    adding logic to the cards card component

--- src/Layout/Dialog/content/Cards.tsx
+++ src/Layout/Dialog/content/Cards.tsx
@@ -2,13 +2,25 @@ import { useAreas } from '../../../context/para/areas';
 
 function Cards ({ id }: { id: number }) {
   let area = useAreas(state => state.active)?.at(-1)?.ui_schema;
+  let card_m = useAreas(state => state.get_Card)(id);
+  let update = useAreas(state => state.update_card);
   return (
     <div>
-      {area?.item.map(item => (
-        <div onClick={() => {
-          
-        }}>{item.type}</div>
-      ))}
+      {area?.item.map(item => {
+        if (item.type == 'tasks') {
+          return (
+            <div
+              onClick={() => {
+                let card = card_m.props.push(item.id)
+                
+                update(id, card);
+              }}
+            >
+              {item.type}
+            </div>
+          );
+        }
+      })}
     </div>
   );
 }
