adeff61af81c421e7304ed5c7195b2954f80c6b6
mohmmedalkhatim <developmentaccun@outlook.com>
2025-07-08 15:34:09 +0200

    fixing a bug on the card_content component

--- src/Layout/CardContent/index.tsx
+++ src/Layout/CardContent/index.tsx
@@ -1,17 +1,17 @@
 import { useEffect, useState } from 'react';
 import AreasList from '../components/List';
-import Editor_card from '../components/Editor';
 import TaskList from '../components/Tasklist';
 import Image from '../components/Image';
 import Calendar from '../components/Calender';
 import { LoadingBar } from '../components/loadingbar/LoadingBar';
+import NoteViewer from '../components/NoteViewer';
 
 type Card_content = { id: number; T: string; props?: any };
 
 function CardContent({ id, T, props }: Card_content) {
   const [map, setMap] = useState(
     new Map([
-      ['editor', <Editor_card content={props} id={id} title={'untitled'} />],
+      ['editor', <NoteViewer id={id} />],
       ['Areaslist', <AreasList id={id} />],
       ['tasks', <TaskList id={id} />],
       ['image', <Image id={id} />],
