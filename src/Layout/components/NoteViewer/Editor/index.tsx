34c4b17ee9f9c731ccdba6656f410d9fb9838baa
mohmmedalkhatim <developmentaccun@outlook.com>
2025-07-09 13:39:42 +0200

    restructuring the Note editor component

---
+++ src/Layout/components/NoteViewer/Editor/index.tsx
@@ -0,0 +1,53 @@
+import { Placeholder } from '@tiptap/extension-placeholder';
+import { useEditor, EditorContent, Editor } from '@tiptap/react';
+import TextAlign from '@tiptap/extension-text-align';
+import StarterKit from '@tiptap/starter-kit';
+import { useEffect, useState } from 'react';
+import { useAreas } from '../../../../context/para/areas';
+import { useNotes } from '../../../../context/para/notes';
+import { Note } from '../../../../types/notes';
+
+function Rich_Editor ({ id, content }: { content: any; id: number }) {
+  const active = useNotes(state => state.active);
+  let get_card = useAreas(state => state.get_Card);
+  const editor = useEditor({
+    extensions: [
+      StarterKit,
+      TextAlign.configure({
+        types: ['paragraph', 'heading'],
+      }),
+      Placeholder.configure({ placeholder: 'start writing..' }),
+    ],
+
+    content: content,
+    editorProps: {
+      attributes: {
+        class: 'editor',
+      },
+    },
+  });
+  useEffect(() => {
+    editor?.chain().focus().run();
+  if (editor) {
+    editor.once('update', e => {
+      const content = editor.getJSON();
+      const description = editor.getText();
+      let card = get_card(id);
+      if (content) {
+        const note = { title: active.title, content, description, id } as Note;
+        update_note(card.props.note_id, note);
+      }
+    });}
+  }, []);
+
+  const update_note = useNotes(state => state.updata_note);
+
+    return (
+      <EditorContent
+        className='editor'
+        placeholder='start writing'
+        editor={editor}
+      />
+    );
+  }
+export default Rich_Editor;
--- src/Layout/components/NoteViewer/index.tsx
+++ src/Layout/components/NoteViewer/index.tsx
@@ -1,62 +1,28 @@
-import { Placeholder } from '@tiptap/extension-placeholder';
-import { useEditor, EditorContent } from '@tiptap/react';
-import TextAlign from '@tiptap/extension-text-align';
-import StarterKit from '@tiptap/starter-kit';
-import { useEffect, useState } from 'react';
-import './style.css';
-import { useNotes } from '../../../context/para/notes';
-import { Note } from '../../../types/notes';
-import { useAreas } from '../../../context/para/areas';
 import { IconLink } from '@tabler/icons-react';
+import { useNotes } from '../../../context/para/notes';
+import Rich_Editor from './Editor';
+import { useEffect, useState } from 'react';
+import { useAreas } from '../../../context/para/areas';
+import './style.css';
 
 const NoteViewer = ({ id }: { id: number }) => {
+  const active_area = useAreas(state => state.active)?.at(-1);
+  const get_note = useNotes(state => state.note);
+  const active = useNotes(state => state.active);
   const [loading, setloading] = useState(true);
-  let active = useNotes(state => state.active);
-  let get_note = useNotes(state => state.note);
-  let active_area = useAreas(state => state.active)?.at(-1);
-  let get_card = useAreas(state => state.get_Card);
-  const editor = useEditor({
-    extensions: [
-      StarterKit,
-      TextAlign.configure({
-        types: ['paragraph', 'heading'],
-      }),
-      Placeholder.configure({ placeholder: 'start writing..' }),
-    ],
+  const get_card = useAreas(state => state.get_Card);
 
-    content: active.content,
-    editorProps: {
-      attributes: {
-        class: 'editor',
-      },
-    },
-  });
   useEffect(() => {
-    editor?.chain().focus().run();
     let card = get_card(id);
-    get_note(card.props.id || active_area?.note_id, setloading);
+    console.log(card.props);
+    get_note(card.props.note_id || active_area?.note_id, setloading);
   }, []);
-  const update = useNotes(state => state.updata_note);
-  if (editor) {
-    editor.once('update', e => {
-      const content = editor.getJSON();
-      const description = editor.getText();
-      let card = get_card(id);
-      if (content) {
-        const note = { title: active.title, content, description, id } as Note;
-        console.log(Number(card.props),active_area?.note_id)
-        update(card.props.note_id || active_area?.note_id, note);
-      }
-    });
+  if (!loading) {
     return (
       <div className=' border-none outline-none'>
-        <EditorContent
-          className='editor'
-          placeholder='start writing'
-          editor={editor}
-        />
-        <div className='absolute bottom-4 right-4' onClick={()=>{}}>
-          <IconLink color='#e2e2e280' size={"1.4rem"}/>
+        <Rich_Editor content={active.content} id={id} />
+        <div className='absolute bottom-4 right-4' onClick={() => {}}>
+          <IconLink color='#e2e2e280' size={'1.4rem'} />
         </div>
       </div>
     );
