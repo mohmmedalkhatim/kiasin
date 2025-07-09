a6dc93fefbde43c73e946fc225f6c38d5ff0d298
mohmmedalkhatim <developmentaccun@outlook.com>
2025-07-09 19:19:50 +0200

    removing unused logic

--- src/Layout/components/NoteViewer/index.tsx
+++ src/Layout/components/NoteViewer/index.tsx
@@ -2,20 +2,14 @@ import { IconLink } from '@tabler/icons-react';
 import { useNotes } from '../../../context/para/notes';
 import Rich_Editor from './Editor';
 import { useEffect, useState } from 'react';
-import { useAreas } from '../../../context/para/areas';
 import './style.css';
 
 const NoteViewer = ({ id }: { id: number }) => {
-  const active_area = useAreas(state => state.active)?.at(-1);
   const get_note = useNotes(state => state.note);
   const active = useNotes(state => state.active);
   const [loading, setloading] = useState(true);
-  const get_card = useAreas(state => state.get_Card);
-
   useEffect(() => {
-    let card = get_card(id);
-    console.log(card.props);
-    get_note(card.props.note_id || active_area?.note_id, setloading);
+    get_note(id , setloading);
   }, []);
   if (!loading) {
     return (
