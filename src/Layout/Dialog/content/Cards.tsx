df4abfa9729d20f5ac739c46597c719cbc20bbcd
mohmmedalkhatim <developmentaccun@outlook.com>
2025-07-09 19:20:46 +0200

    working on the dialog content

--- src/Layout/Dialog/content/Cards.tsx
+++ src/Layout/Dialog/content/Cards.tsx
@@ -2,8 +2,8 @@ import { useAreas } from '../../../context/para/areas';
 
 function Cards ({ id }: { id: number }) {
   let area = useAreas(state => state.active)?.at(-1)?.ui_schema;
-  let card_m = useAreas(state => state.get_Card)(id);
   let update = useAreas(state => state.update_card);
+  let get_card = useAreas(state => state.get_Card);
   return (
     <div>
       {area?.item.map(item => {
@@ -11,7 +11,7 @@ function Cards ({ id }: { id: number }) {
           return (
             <div
               onClick={() => {
-                let card = card_m.props.push(item.id);
+                let card = get_card(id).props.push(item.id)
                 update(id, card);
               }}
             >
