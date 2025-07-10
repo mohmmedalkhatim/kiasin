1c9647506a37b7dd2fe0337490d9ae3592252a23
mohmmedalkhatim <developmentaccun@outlook.com>
2025-07-10 20:07:59 +0200

    removing unused logic

--- src/note/Editor/index.tsx
+++ src/note/Editor/index.tsx
@@ -1,15 +1,9 @@
 import { EditorContent, Editor } from '@tiptap/react';
 import { Toolbar } from './toolbar';
-import { useState } from 'react';
 import './style.css';
 
 const RichTextEditor = ({ editor, id }: { editor: Editor; id: number }) => {
-
   if (editor) {
-
-    editor.on('destroy', (e) => {
-      editor.getJSON();
-    });
     return (
       <div className="editor_container">
         <Toolbar editor={editor} />
