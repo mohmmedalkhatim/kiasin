1cdf0c3f0afe16891b2c01b89bfe797ea2f9417a
mohmmedalkhatim <developmentaccun@outlook.com>
2025-07-10 20:07:40 +0200

    formatting the areas component

--- src/Layout/Dialog/content/Area/Areas.tsx
+++ src/Layout/Dialog/content/Area/Areas.tsx
@@ -4,12 +4,19 @@ import AreaCard from './Card';
 function Areas ({ id }: { id: number }) {
   let active = useAreas(state => state.active)?.at(-1);
   let list = useAreas(state => state.list);
-  let get_card = useAreas(state=>state.get_Card)
-  return <div className='boxs_grid '>{list.map((item)=>{
-    let card= get_card(id)
-    if(item.id != active?.id && !(card.props.list as number[]).includes(item.id)){
-        return(<AreaCard card_id={id} id={item.id}></AreaCard>)
-    }
-  })}</div>;
+  let get_card = useAreas(state => state.get_Card);
+  return (
+    <div className='boxs_grid '>
+      {list.map(item => {
+        let card = get_card(id);
+        if (
+          item.id != active?.id &&
+          !(card.props.list as number[]).includes(item.id)
+        ) {
+          return <AreaCard card_id={id} id={item.id}></AreaCard>;
+        }
+      })}
+    </div>
+  );
 }
 export default Areas;
