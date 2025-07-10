e849129fcd58b3a7ccc6e0d93db1700eb2a2e5ab
mohmmedalkhatim <developmentaccun@outlook.com>
2025-07-10 20:03:55 +0200

    formatting the aside component

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -7,11 +7,10 @@ function Aside () {
   const active = useAreas(state => state.editable);
   return (
     <>
-      
-        <nav className={!active ?"navbar":"navbar -left-20"}>
-          <Profile_link />
-          <Links />
-        </nav>
+      <nav className={!active ? 'navbar' : 'navbar -left-20'}>
+        <Profile_link />
+        <Links />
+      </nav>
     </>
   );
 }
