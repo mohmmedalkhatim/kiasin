ce58ac329a74aea1c4fe5c704dcefe9545f40ef7
mohmmedalkhatim <developmentaccun@outlook.com>
2025-07-10 20:06:40 +0200

    formatting the content component

--- src/Layout/Dialog/content/index.tsx
+++ src/Layout/Dialog/content/index.tsx
@@ -1,15 +1,15 @@
 import { useState } from 'react';
-import Editor from '../../components/Editor';
 import Cards from './Cards';
 import Areas from './Area/Areas';
+import NoteViewer from '../../components/NoteViewer';
 
 function Content ({ props, mode }: { props: any; mode: string }) {
-  let [element, setElement] = useState(
+  let [element] = useState(
     new Map([
-      ['dialog_note', <Editor id={props.id} content={''} title={''} />],
+      ['dialog_note', <NoteViewer id={props.note_id} />],
       ['dialog_links', <Cards id={props.id} />],
       ['dialog_areas', <Areas id={props.id} />],
-      ['dialog_tasks', <Areas id={props.id} />],
+      ['dialog_tasks', <Cards id={props.id} />],
       ['default', <div>no Content</div>],
     ])
   );
