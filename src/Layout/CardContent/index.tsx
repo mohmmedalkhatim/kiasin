2901873ea2d8839feabd31636d355fc2ddb997c5
mohmmedalkhatim <developmentaccun@outlook.com>
2025-07-10 20:05:30 +0200

    adding new logic to the card content to fix a bug

--- src/Layout/CardContent/index.tsx
+++ src/Layout/CardContent/index.tsx
@@ -5,13 +5,15 @@ import Image from '../components/Image';
 import Calendar from '../components/Calender';
 import { LoadingBar } from '../components/loadingbar/LoadingBar';
 import NoteViewer from '../components/NoteViewer';
+import { useAreas } from '../../context/para/areas';
 
 type Card_content = { id: number; T: string; props?: any };
 
-function CardContent({ id, T, props }: Card_content) {
+function CardContent ({ id, T, props }: Card_content) {
+  let active = useAreas(State => State.active)?.at(-1);
   const [map, setMap] = useState(
     new Map([
-      ['editor', <NoteViewer id={id} />],
+      ['editor', <NoteViewer id={id || active?.note_id || 0} />],
       ['Areaslist', <AreasList id={id} />],
       ['tasks', <TaskList id={id} />],
       ['image', <Image id={id} />],
