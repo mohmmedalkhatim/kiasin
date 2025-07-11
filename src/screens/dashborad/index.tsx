7dd74e5ef4ca3535771447e67b026acfd15507c3
mohmmedalkhatim <developmentaccun@outlook.com>
2025-07-11 19:57:57 +0200

    removing the react query usege

--- src/screens/dashborad/index.tsx
+++ src/screens/dashborad/index.tsx
@@ -2,32 +2,25 @@ import './style.css';
 import Layout from '../../Layout';
 import { invoke } from '@tauri-apps/api/core';
 import { Area } from '../../types/area';
-import { useQuery } from '@tanstack/react-query';
 import { useAreas } from '../../context/para/areas';
 import Header from '../../components/Headers/Area_Header';
-import { Store } from 'tauri-plugin-store-api';
+import { useEffect, useState } from 'react';
 
 function DashBoard ({}: {}) {
   const setActive = useAreas(state => state.update_active_area);
-  const editable = useAreas(state=>state.editable);
-
-  const dashboard_store = new Store('./dashboard');
-
-  const { data, error, isLoading } = useQuery({
-    queryKey: ['area'],
-    queryFn: async () => invoke<Area>("dashboard"),
-  });
-  if (isLoading) return <main className='content'>Loading...</main>;
-  if (error) {
-    console.log(error)
-    return <main className='content'>Error: {error.message}</main>;
-  }
-  if (data) {
-    setActive(data);
+  const editable = useAreas(state => state.editable);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    invoke<Area>('dashboard').then(res => {
+      setActive(res);
+      setLoading(true);
+    });
+  }, []);
+  if (loading) {
     return (
       <>
         <Header />
-        <main className={!editable?"content":"mt-[5rem] mr-[16rem]"}>
+        <main className={!editable ? 'content' : 'mt-[5rem] mr-[16rem]'}>
           <Layout />
         </main>
       </>
