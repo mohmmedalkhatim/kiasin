476ab1052e213afe90e6c53b3c791847b7cbdb9d
mohmmedalkhatim <developmentaccun@outlook.com>
2025-07-23 18:16:21 +0200

    adding the logic to close the bubble menu when the user don't want it

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,12 @@ import Navbar from './components/Navbar';
 import { useNotes } from './context/para/notes';
 import Dialog from './Layout/Dialog';
 import BubbleMenu from './App_bubble';
+import { useBubbleMenu } from './context/para/BubbleMenu';
 
 function App() {
   const init = useAreas((state) => state.init);
-  let initNotes = useNotes((state) => state.init);
-
+  const initNotes = useNotes((state) => state.init);
+  const close  = useBubbleMenu(state=>state.close)
   useEffect(() => {
     init();
     initNotes();
@@ -21,10 +22,12 @@ function App() {
   return (
     <>
       <BubbleMenu />
-      <Outlet />
-      <Navbar />
-      <Aside />
-      <Dialog />
+      <div onClick={close}>
+        <Outlet />
+        <Navbar />
+        <Aside />
+        <Dialog />
+      </div>
     </>
   );
 }
