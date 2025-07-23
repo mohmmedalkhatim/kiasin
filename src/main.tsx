dc4ead9939374f344ed29d30e02e75f55b417154
mohmmedalkhatim <developmentaccun@outlook.com>
2025-07-23 18:16:55 +0200

    working on the shortcut logic

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,8 @@ import ScrollTrigger from 'gsap/ScrollTrigger';
 import { RouterProvider } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './styles.css';
+import { AlteShortCuts } from './shortcuts';
+import { useAreas } from './context/para/areas';
 
 const lenis = new Lenis();
 
@@ -14,6 +16,14 @@ lenis.on('scroll', ScrollTrigger.update);
 gsap.ticker.add((time) => {
   lenis.raf(time * 3000);
 });
+
+document.addEventListener("keydown",(e)=>{
+  if(e.altKey){
+    if(e.key == "e"){
+      useAreas.getState().toggleEditable()
+    }
+  }
+})
 gsap.ticker.lagSmoothing(0);
 
 gsap.registerPlugin(ScrollTrigger);
