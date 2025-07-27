bb733d1880bf45ec1e19f57c0ae32e2811423ca2
mohmmedalkhatim <developmentaccun@outlook.com>
2025-07-27 18:05:46 +0200

    fixing a typo issus and work on the display size

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -3,15 +3,15 @@ import Links from './Links';
 import Profile_link from './profile';
 import { useAreas } from '../../context/para/areas';
 
-function Aside () {
+function Navbar () {
   const active = useAreas(state => state.editable);
   return (
     <>
-      <nav className={!active ? 'navbar' : 'navbar -left-20'}>
+      <nav className={!active ? 'navbar' : 'navbar -left-24'}>
         <Profile_link />
         <Links />
       </nav>
     </>
   );
 }
-export default Aside;
+export default Navbar;
