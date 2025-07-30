fb0bb3ebf542c0da0af66e473627721a2a3fb517
mohmmedalkhatim <developmentaccun@outlook.com>
2025-07-30 14:38:26 +0200

    fixing a bug on the areas list

--- src/Layout/components/List/index.tsx
+++ src/Layout/components/List/index.tsx
@@ -1,45 +1,33 @@
 import { IconLayersIntersect } from '@tabler/icons-react';
 import Card from '../../../components/Cards/area_card';
-import { useAreas } from '../../../context/para/areas';
-import { useEffect, useState } from 'react';
-import { useDebounce } from 'react-use';
-import { useLayoutDialog } from '../../../context/para/Dialog';
-import Input from '../../../components/Input';
+import useAreaList from './hook';
 
 function AreasList({ id }: { id: number }) {
-  let list = useAreas(state => state.list);
-  let card = useAreas(state => state.get_Card)(id);
-  let update_card = useAreas(state => state.update_card);
-  let changeMode = useLayoutDialog(state => state.changeMode)
-  let [areas, setAreas] = useState<number[]>();
-  useEffect(
-    () => {
-      if(!areas){
-      setAreas(card.props.list);
-      }
-    },    [list]
-  );
-  return (
-    <div className='p-4'>
-      <div className='px-4 pb-2 flex items-center justify-between'>
-        <div className=''>
-          <input
-            className='outline-none border-none focus:border p-0 text-sm'
-            placeholder='untitled'
-            value={card.props.name} onChange={(e) => update_card(id, { ...card, props: { ...card.props, name: e.target.value } })}></input>
+  let { areas, update_card, card, changeMode } = useAreaList(id)
+  console.log(areas)
+  if (areas) {
+    return (
+      <div className='p-4'>
+        <div className='px-4 pb-2 flex items-center justify-between'>
+          <div className=''>
+            <input
+              className='outline-none border-none focus:border p-0 text-sm'
+              placeholder='untitled'
+              value={card.props.name} onChange={(e) => update_card(id, { ...card, props: { ...card.props, name: e.target.value } })}></input>
+          </div>
+          <div onClick={() => changeMode("dialog_areas", { id })}>
+            <IconLayersIntersect />
+          </div>
         </div>
-        <div onClick={() => changeMode("dialog_areas", { id })}>
-          <IconLayersIntersect />
+        <div className='m_border  mb-3 '></div>
+        <div className='boxs_grid'>
+          {areas.map(item => (
+            <Card key={item} id={item} />
+          ))}
         </div>
       </div>
-      <div className='m_border  mb-3 '></div>
-      <div className='boxs_grid'>
-        {areas?.map(item => (
-          <Card key={item} id={item} />
-        ))}
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default AreasList;
