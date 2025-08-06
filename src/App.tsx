e4f4875f926f1b86dc72ce39e4966608ae653b33
mohmmedalkhatim <developmentaccun@outlook.com>
2025-08-06 11:16:50 +0200

    removing unused object

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,9 @@ import { Outlet } from 'react-router-dom';
 import Aside from './components/Aside';
 import './App.css';
 import { useEffect } from 'react';
-import { useAuth } from './context/User';
 import { useAreas } from './context/para/areas';
 import Navbar from './components/Navbar';
 import { useNotes } from './context/para/notes';
-import { useMeasure } from 'react-use';
 import Dialog from './Layout/Dialog';
 function App() {
   const init = useAreas((state) => state.init);
