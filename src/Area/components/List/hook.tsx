95c09196c64fdd9553c9ef1813d9713db71fd2b9
mohmmedalkhatim <developmentaccun@outlook.com>
2025-08-09 18:25:06 +0200

    fixing an updating issus with the area list

--- src/Area/components/List/hook.tsx
+++ src/Area/components/List/hook.tsx
@@ -1,18 +1,20 @@
 import { useState, useEffect } from "react";
 import { useAreas } from "../../../context/para/areas";
 import { useLayoutDialog } from "../../../context/para/Dialog";
+import { useDebounce } from "react-use";
 
-function useAreaList(id:number) {
+function useAreaList(id: number) {
     let list = useAreas(state => state.list);
     let card = useAreas(state => state.get_Card)(id);
     let update_card = useAreas(state => state.update_card);
     let changeMode = useLayoutDialog(state => state.changeMode)
     let [areas, setAreas] = useState<number[]>();
-    useEffect(
+    let active = useAreas(state => state.active)?.at(-1)
+    useDebounce(
         () => {
             setAreas(card.props.list);
-        }, []
+        }, 40, [active]
     );
-    return { list, update_card,card, changeMode, areas }
+    return { list, update_card, card, changeMode, areas }
 }
 export default useAreaList
