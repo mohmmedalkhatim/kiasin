4f5aa0f8f59f1048fa54b8e4861c8a3c19e4761a
mohmmedalkhatim <developmentaccun@outlook.com>
2025-08-12 06:15:43 +0200

    adding the bubble menu to the note view

--- src/Area/Dialog/content/NoteViewer/Editor/index.tsx
+++ src/Area/Dialog/content/NoteViewer/Editor/index.tsx
@@ -1,38 +1,41 @@
-import { Editor, EditorContent } from "@tiptap/react";
+import { EditorContent } from "@tiptap/react";
 import { Note } from "../../../../../types/notes";
 import { Placeholder } from '@tiptap/extension-placeholder';
 import { useEditor } from '@tiptap/react';
 import TextAlign from '@tiptap/extension-text-align';
 import StarterKit from '@tiptap/starter-kit';
 import NoteHeader from "../../../../../note/NoteHeader/indxe";
+import Underline from "@tiptap/extension-underline";
+import Highlight from '@tiptap/extension-highlight';
+import textStyle from '@tiptap/extension-text-style';
+import '../style.css';
+import { Toolbar } from "../../../../../note/Editor/toolbar";
 
 function Rich_Editor({ note }: { note: Note }) {
   const editor = useEditor({
     extensions: [
       StarterKit,
+      Underline,
       TextAlign.configure({
-        types: ['paragraph', 'heading'],
+        types: ['heading', 'paragraph'],
       }),
-      Placeholder.configure({ placeholder: 'start writing..' }),
+      Highlight.configure(),
+      textStyle,
+      Placeholder.configure({ placeholder: 'start writing ...' }),
     ],
-
-    content: (async () => {
-      if (note) {
-        return note.content
-      }
-    })(),
+    content:note.content,
     editorProps: {
       attributes: {
         class: 'editor',
       },
     },
   });
-
   if (editor) {
     return (
       <div className="relative">
         <NoteHeader editor={editor} hide_background id={note.id} Title={note.title}></NoteHeader>
-        <div className="px-20">
+        <div className="px-24 py-6">
+        <Toolbar editor={editor}/>
           <EditorContent
             className='editor'
             placeholder='start writing'
