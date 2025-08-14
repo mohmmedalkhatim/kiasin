17f0754b0caf6b7589172288d78bdfddec9b60e5
mohmmedalkhatim <developmentaccun@outlook.com>
2025-08-14 17:45:53 +0200

    create the database control command

--- src-tauri/src/app/commands/Database/functions.rs
+++ src-tauri/src/app/commands/Database/functions.rs
@@ -1,8 +1,8 @@
-
-
 mod create;
 mod retrieve;
 mod update;
 mod delete;
 
 
+pub  use create::create;
+pub  use retrieve::get;
--- src-tauri/src/app/commands/Database/mod.rs
+++ src-tauri/src/app/commands/Database/mod.rs
@@ -1,4 +1,44 @@
+use crate::DbConnection;
+use async_std::sync::Mutex;
+use migration::entities::db::Model;
+use objects::Payload;
+use std::sync::Arc;
+use tauri::{ipc::Channel, AppHandle, State};
+mod functions;
+mod objects;
+
 #[tauri::command]
-pub async fn database_control() -> Result<(), String> {
-    Ok(())
+pub async fn database_control(
+    app: AppHandle,
+    payload: Payload,
+    channel: Channel<Vec<Model>>,
+    data: State<'_, Arc<Mutex<DbConnection>>>,
+) -> Result<(), String> {
+    let db = data.lock().await.db.clone().unwrap();
+    let database = payload.item;
+    match payload.command.as_str() {
+        "create" => match functions::create(app, database, &db).await {
+            Ok(id) => match functions::get(id, &db).await {
+                Ok(item) => {
+                    let _ = channel.send(vec![item]);
+                    Ok(())
+                }
+                Err(e) => Err(e.to_string()),
+            },
+            Err(e) => Err(e.to_string()),
+        },
+        "delete" => {
+            todo!()
+        }
+        "update" => {
+            todo!()
+        }
+        "list" => {
+            todo!()
+        }
+        "one" => {
+            todo!()
+        }
+        _ => Ok(()),
+    }
 }
