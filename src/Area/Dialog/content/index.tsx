669e531a19e3d2d4836dbded31a7d3ab4c0cb4bb
mohmmedalkhatim <developmentaccun@outlook.com>
2025-08-15 21:03:47 +0200

    the setting content to the main dialog

--- src/Area/Dialog/content/index.tsx
+++ src/Area/Dialog/content/index.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Cards from './Cards';
 import Areas from './Area/Areas';
 import NoteViewer from '../content/NoteViewer';
+import Setting from './Setting';
 
 function Content ({ props, mode }: { props: any; mode: string }) {
   let [element] = useState(
@@ -10,6 +11,7 @@ function Content ({ props, mode }: { props: any; mode: string }) {
       ['dialog_links', <Cards id={props.id} />],
       ['dialog_areas', <Areas id={props.id} />],
       ['dialog_tasks', <Cards id={props.id} />],
+      ['dialog_setting', <Setting />],
       ['default', <div>no Content</div>],
     ])
   );
