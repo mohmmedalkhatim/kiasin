456f316c0c3b6ef77fcb416e6694219df1ca023f
mohmmedalkhatim <developmentaccun@outlook.com>
2025-08-18 08:06:14 +0200

    working on the command payload

--- src-tauri/src/app/commands/Database/objects.rs
+++ src-tauri/src/app/commands/Database/objects.rs
@@ -4,11 +4,13 @@ use serde_json::Value;
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Payload {
     pub command: String,
-    pub item: Database,
+    pub item: Option<Database>,
+    pub id: Option<i32>,
+    pub ids: Option<Vec<i32>>,
 }
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Database {
-    pub id: u32,
+    pub id: i32,
     pub name: String,
     pub data: Value,
 }
