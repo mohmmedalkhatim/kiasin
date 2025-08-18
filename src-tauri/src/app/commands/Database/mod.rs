3632bca92ed960dcbae3b1551e400491c1f244c8
mohmmedalkhatim <developmentaccun@outlook.com>
2025-08-18 08:05:43 +0200

    complating the Database_control command

--- src-tauri/src/app/commands/Database/mod.rs
+++ src-tauri/src/app/commands/Database/mod.rs
@@ -3,13 +3,12 @@ use async_std::sync::Mutex;
 use migration::entities::db::Model;
 use objects::Payload;
 use std::sync::Arc;
-use tauri::{ipc::Channel, AppHandle, State};
+use tauri::{ipc::Channel, State};
 mod functions;
 mod objects;
 
 #[tauri::command]
 pub async fn database_control(
-    app: AppHandle,
     payload: Payload,
     channel: Channel<Vec<Model>>,
     data: State<'_, Arc<Mutex<DbConnection>>>,
@@ -17,7 +16,7 @@ pub async fn database_control(
     let db = data.lock().await.db.clone().unwrap();
     let database = payload.item;
     match payload.command.as_str() {
-        "create" => match functions::create(app, database, &db).await {
+        "create" => match functions::create(database.unwrap(), &db).await {
             Ok(id) => match functions::get(id, &db).await {
                 Ok(item) => {
                     let _ = channel.send(vec![item]);
@@ -27,18 +26,42 @@ pub async fn database_control(
             },
             Err(e) => Err(e.to_string()),
         },
-        "delete" => {
-            todo!()
-        }
-        "update" => {
-            todo!()
-        }
-        "list" => {
-            todo!()
-        }
-        "one" => {
-            todo!()
-        }
+        "delete" => match payload.id {
+            Some(id) => match functions::delete(id, &db).await {
+                Ok(_done) => Ok(()),
+                Err(e) => Err(e.to_string()),
+            },
+            None => Err("you have to send an id".to_string()),
+        },
+        "update" => match database {
+            Some(model) => {
+                let _ = functions::update(model, &db);
+                Ok(())
+            }
+            None => Err("you have to add a payload".to_string()),
+        },
+        "list" => match payload.ids {
+            Some(ids) => match functions::list(ids, &db).await {
+                Ok(list) => {
+                    let _ = channel.send(list);
+                    Ok(())
+                }
+                Err(e) => {
+                    return Err(e.to_string());
+                }
+            },
+            None => todo!(),
+        },
+        "one" => match payload.id {
+            Some(id) => match functions::get(id, &db).await {
+                Ok(model) => {
+                    let _ = channel.send(vec![model]);
+                    Ok(())
+                }
+                Err(massage) => Err(massage.to_string()),
+            },
+            None => Err("you have to add an id".to_string()),
+        },
         _ => Ok(()),
     }
 }
