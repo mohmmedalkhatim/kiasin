b697777197217ba62d910999202382474c87e26b
mohmmedalkhatim <developmentaccun@outlook.com>
2025-08-18 08:06:49 +0200

    complating the delete function

---
+++ src-tauri/src/app/commands/Database/functions/delete.rs
@@ -0,0 +1,6 @@
+use migration::entities::db::Entity;
+use sea_orm::{DatabaseConnection, DbErr, DeleteResult, EntityTrait};
+
+pub async fn delete(id: i32, db: &DatabaseConnection) -> Result<DeleteResult    , DbErr> {
+    Ok(Entity::delete_by_id(id).exec(db).await?)
+}
