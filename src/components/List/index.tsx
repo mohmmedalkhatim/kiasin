e06a73f5508ceb6b7b1f1d6595cfdce915ad261f
mohmmedalkhatim <developmentaccun@outlook.com>
2025-08-19 08:58:08 +0200

    adding more padding to the area list

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -2,7 +2,7 @@ import AreaCard from '../Cards/area_card';
 
 function AreasList(props: { list: number[] }) {
   return (
-    <div className="p-4">
+    <div className="p-16">
       <div className="boxs_grid">
         {props.list.map((item) => (
           <AreaCard key={item} id={item} />
