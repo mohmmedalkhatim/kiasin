ba2ace66cb099e0e87f097a78be839d0fb3f389d
mohmmedalkhatim <developmentaccun@outlook.com>
2025-08-19 08:57:42 +0200

    fixing a bug on the sort card

--- src/Area/Navbar/CardSort/index.tsx
+++ src/Area/Navbar/CardSort/index.tsx
@@ -2,20 +2,22 @@ import { useSortable } from '@dnd-kit/sortable';
 import { IconGridDots } from '@tabler/icons-react';
 import { CSS } from '@dnd-kit/utilities';
 import { useAreas } from '../../../context/para/areas';
-function CardSort ({ id }: { id: string }) {
+
+
+function CardSort({ id }: { id: string }) {
   const { attributes, listeners, setNodeRef, transform, transition } =
     useSortable({ id });
   const style = {
     transform: CSS.Transform.toString(transform),
     transition,
   };
-  const card = useAreas(state=>state.get_Card)(Number(id));
+  const card = useAreas(state => state.get_Card)(Number(id));
   return (
     <div ref={setNodeRef} className='flex item-center rounded-xs gap-4 m_border py-3 px-4' style={style} {...attributes}>
       <div className='cursor-grab' {...listeners}>
         <IconGridDots size={"1rem"} />
       </div>
-      <div className='select-none text-sm h-auto'>{card.type}</div>
+      <div className='select-none text-sm h-auto'>{card?.type}</div>
     </div>
   );
 }
