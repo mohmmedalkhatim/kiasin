7dddfc569ca009b3ac4b30b5a644d57c95e46014
mohmmedalkhatim <developmentaccun@outlook.com>
2025-08-19 08:58:46 +0200

    adding a button to navbar to open the setting panel

--- src/components/Navbar/Links/index.tsx
+++ src/components/Navbar/Links/index.tsx
@@ -1,24 +1,33 @@
-import { Link } from 'react-router-dom';
 import {
   IconDashboard,
   IconDatabase,
   IconLayersIntersect,
   IconNotes,
+  IconSettings,
 } from '@tabler/icons-react';
 import Page from './link';
+import { useLayoutDialog } from '../../../context/para/Dialog';
 
 function Links() {
+  const { changeMode } = useLayoutDialog()
   const arr = [
     { name: 'dashboard', link: '/', Icon: <IconDashboard /> },
     { name: 'areas', link: '/areas', Icon: <IconLayersIntersect /> },
     { name: 'notes', link: '/notes', Icon: <IconNotes /> },
-    { name: 'Databases', link: '/', Icon: <IconDatabase /> },
+    { name: 'Databases', link: '/Databases', Icon: <IconDatabase /> },
   ];
   return (
-    <div className="links">
-      {arr.map((item, i) => (
-        <Page id={i} key={i} {...item} />
-      ))}
+    <div className='flex flex-col item-center justify-between h-full pb-20'>
+      <div className="links">
+        {arr.map((item, i) => (
+          <Page id={i} key={i} {...item} />
+        ))}
+      </div>
+      <div className='link group rounded z-40' onClick={() => changeMode("dialog_setting", { id: 0 })}>
+        <div>
+          <IconSettings />
+        </div>
+      </div>
     </div>
   );
 }
