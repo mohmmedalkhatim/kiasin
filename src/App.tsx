d977ccc705f71c1af61626fc17850fbd9f168819
mohmmedalkhatim <developmentaccun@outlook.com>
2025-08-22 10:06:28 +0200

    database fetching when starting the app

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,19 @@ import { useNotes } from './context/para/notes';
 import Dialog from './Area/Dialog';
 import BubbleMenu from './App_bubble';
 import { useBubbleMenu } from './context/para/BubbleMenu';
+import { useDatabase } from './context/para/database';
 
 
 function App() {
   const init = useAreas((state) => state.init);
+  const init_db = useDatabase(state=>state.init)
   const initNotes = useNotes((state) => state.init);
   const close_bubble = useBubbleMenu(state => state.close)
   const close = async () => {
     close_bubble()
   }
   useEffect(() => {
+    init_db()
     init();
     initNotes();
   }, []);
