01c760deca92e47d66af0130dbfba99ebf6a49ba
mohmmedalkhatim <developmentaccun@outlook.com>
2025-08-22 10:09:23 +0200

    fixing spaces on the aside component

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -4,6 +4,7 @@ import { IconChevronsRight } from '@tabler/icons-react';
 import Button from '../Button';
 import Notes_templates from './notes_templates';
 import Areas_templates from './areas_templates';
+import DBConfig from './DbConfig';
 
 function Aside() {
   const active = useAside((state) => state.active);
@@ -11,15 +12,16 @@ function Aside() {
   const inside = new Map([
     ['notes', <Notes_templates />],
     ['areas', <Areas_templates />],
+    ['DBConfig', <DBConfig />],
   ]);
-  const t = useAside((state) => state.t);
+  const type = useAside((state) => state.Type);
   return (
     <>
     <aside className={`${active && 'hide'} Aside`}>
       <div className="aside_header">
-        <Button children={<IconChevronsRight />} onClick={() => toggle('')} />
+        <Button children={<IconChevronsRight />} className='p-2' onClick={() => toggle('')} />
       </div>
-      <div className="content_container">{inside.get(t)}</div>
+      <div className="content_container">{inside.get(type)}</div>
     </aside>
     </>
   );
