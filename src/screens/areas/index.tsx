fbfa18cf6d1ee6730e1b9c3df8f1a7685b130388
mohmmedalkhatim <developmentaccun@outlook.com>
2025-08-27 19:58:40 +0200

    adding the header component and working on the page spacing

--- src/screens/areas/index.tsx
+++ src/screens/areas/index.tsx
@@ -2,26 +2,30 @@ import AreaCard from '../../components/Cards/area_card';
 import { IconPlus } from '@tabler/icons-react';
 import { useAreas } from '../../context/para/areas';
 import { useAside } from '../../context/aside';
+import Areas_header from './header';
 
 function Areas() {
   const list = useAreas((state) => state.list);
   const toggle = useAside((state) => state.toggle);
   return (
-    <main className="content">
-      <div className="boxs_grid">
-        {list.map((item) => {
-          return (
+    <>
+      <Areas_header />
+      <main className="content">
+        <div className="boxs_grid p-4">
+          {list.map((item) => {
+            return (
               <AreaCard id={item.id} />
-          );
-        })}
-        <button
-          className="flex items-center justify-center"
-          onClick={() => toggle('areas')}
-        >
-          <IconPlus />
-        </button>
-      </div>
-    </main>
+            );
+          })}
+          <button
+            className="flex items-center justify-center"
+            onClick={() => toggle('areas')}
+          >
+            <IconPlus />
+          </button>
+        </div>
+      </main>
+    </>
   );
 }
 
