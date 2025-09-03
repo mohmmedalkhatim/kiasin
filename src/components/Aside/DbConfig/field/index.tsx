1c6827ed02deacca1a8676bd3f1a4f13c67c5840
mohmmedalkhatim <developmentaccun@outlook.com>
2025-09-03 11:33:48 +0200

    creating the field component and working on teh aside style and DbConfig component

--- src/components/Aside/DbConfig/field/index.tsx
+++ src/components/Aside/DbConfig/field/index.tsx
@@ -1,7 +1,26 @@
-function Field() {
-    
+import { IconChevronDown } from "@tabler/icons-react"
+import { Dispatch, SetStateAction, useRef, useState } from "react"
+
+function Field({ name, DB_ID, list, setValue, value }: { name: string, DB_ID: number, list: string[], value: string, setValue: Dispatch<SetStateAction<string>> }) {
+  let [opened, setOpened] = useState(false)
+  let ref = useRef(name)
   return (
-    <div>Field</div>
+    <>
+      <div className="relative">
+        <div onClick={() => setOpened(state=>!state)} role="combobox" className="m_border z-20 relative flex items-center justify-between  rounded-none p-3 " defaultValue={name}>
+          <div>{value}</div>
+          <div><IconChevronDown /></div>
+        </div>
+        <div className="absolute w-full right-0 bg-[#181818] z-30">
+          {opened && <div >
+            {list.map((option) => (<div role="option" onClick={() => {
+              setValue(option)
+              setOpened(false)
+            }} className="bg-[#181818] m_border p-3 z-60 hover:bg-[#323232]">{option}</div>))}
+          </div>}
+        </div>
+      </div>
+    </>
   )
 }
 export default Field
--- src/components/Aside/DbConfig/index.tsx
+++ src/components/Aside/DbConfig/index.tsx
@@ -1,11 +1,14 @@
 import { useState, useEffect, useRef } from "react";
-import { DB, DB_DTO, useDatabase } from "../../../context/para/database";
+import { DB, useDatabase } from "../../../context/para/database";
 import { useAside } from "../../../context/aside";
+import Field from "./field";
 
 function DBConfig() {
   const [DataB, setDataB] = useState<DB>();
   const one = useDatabase(state => state.get);
   const id = useAside(state => state.id);
+  const [value_one, setValue_one] = useState("")
+  const [value, setValue] = useState("")
   useEffect(() => {
     one(Number(id), setDataB)
   }, [id])
@@ -13,11 +16,11 @@ function DBConfig() {
     let map = new Map(DataB.data)
     return (
       <div className="w-full flex flex-col items-center">
-        <div className="w-full">
-          {DataB.fields
-            .map((value) => (
-              value
-            ))}
+        <div className="w-full flex flex-col gap-4">
+          {DataB.fields.map((name) => (<>
+            <Field name={value} list={["hello", "world"]} value={value} setValue={setValue} DB_ID={id} />
+          </>))}
+          <div></div>
         </div>
       </div>
     )
