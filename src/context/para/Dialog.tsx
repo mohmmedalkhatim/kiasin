251bfcb34052029118e2813d9d43abdceb763ee0
mohmmedalkhatim <developmentaccun@outlook.com>
2025-09-15 18:16:38 +0200

    adding an option to the dialog component

--- src/context/para/Dialog.tsx
+++ src/context/para/Dialog.tsx
@@ -1,11 +1,11 @@
 import { create } from 'zustand';
 
 interface Dialog {
-  mode: 'dialog_note' | 'dialog_calender' | 'dialog_links' | 'dialog_areas' | 'dialog_tasks'| 'dialog_setting';
+  mode: 'dialog_note' | 'dialog_calender' | 'dialog_links' | 'dialog_areas' | 'dialog_tasks'| 'dialog_setting' | 'dialog_notes';
   state: boolean;
   props: { id: number };
   changeMode: (
-    mode: 'dialog_note' | 'dialog_calender' | 'dialog_links' | 'dialog_areas' | 'dialog_tasks'| 'dialog_setting',
+    mode: 'dialog_note' | 'dialog_calender' | 'dialog_links' | 'dialog_areas' | 'dialog_tasks'| 'dialog_setting' | 'dialog_notes',
     props: { id: number }
   ) => void;
   toggle: () => void;
