a96ed005d6547abb991a800679008f97d6a9ea35
mohmmedalkhatim <developmentaccun@outlook.com>
2025-09-24 08:52:55 +0200

    working on the database page and cards

--- src/screens/Databases/header/index.tsx
+++ src/screens/Databases/header/index.tsx
@@ -1,12 +1,14 @@
 import { IconDatabasePlus, IconDatabaseSearch } from "@tabler/icons-react"
 import Button from "../../../components/Button"
 import Input from "../../../components/Input"
+import { useDatabase } from "../../../context/para/database"
 
 function Dbs_Header() {
+    const create_db =  useDatabase(state=>state.create)
     return (
         <header>
             <Input placeholder="search databases" icon={<IconDatabaseSearch size={"1.1rem"} stroke={"2px"} />} />
-            <Button size="sm" className="text-[13px] py-2">create<IconDatabasePlus size={"1.1rem"} /></Button>
+            <Button size="sm" className="text-[13px] py-1" onClick={()=>create_db()}>create<IconDatabasePlus size={"1.1rem"} /></Button>
         </header>
     )
 }
--- src/screens/Databases/index.tsx
+++ src/screens/Databases/index.tsx
@@ -1,11 +1,19 @@
+import { useDatabase } from "../../context/para/database"
+import DbCard from "./DbCard"
 import Dbs_Header from "./header"
-import './style.css'
 
 function Databases() {
+    let list = useDatabase(state => state.list)
     return (
         <>
             <Dbs_Header />
-            <div className="content">Databases</div>
+            <div className="content p-8 boxs_grid">{
+                list.map(item => {
+                    console.log(item)
+                    return (<DbCard key={item.id} name={item.name} id={item.id} />)
+                })
+            }
+            </div>
         </>
     )
 }
