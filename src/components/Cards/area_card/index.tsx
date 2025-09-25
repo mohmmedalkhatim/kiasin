7fb67abe3115891fc76ad78a6d5516201a832b73
mohmmedalkhatim <developmentaccun@outlook.com>
2025-09-25 08:47:06 +0200

    removing shadows from the area card

--- src/components/Cards/area_card/index.tsx
+++ src/components/Cards/area_card/index.tsx
@@ -16,10 +16,10 @@ const AreaCard = ({ id }: { id: number }) => {
   if (Area) {
     return (
       <Link to={`/Area/${Area.id}`} viewTransition>
-        <div className='relative flex flex-col rounded-md m_border bg-clip-border text-gray-700 shadow-md'>
+        <div className='relative flex flex-col rounded-md m_border bg-clip-border text-gray-700'>
           <div
             style={{ backgroundImage: `url()` }}
-            className='relative h-[6rem] overflow-hidden bg-blue-gray-500 bg-clip-border text-white shadow-lg shadow-blue-gray-500/40 bg-gradient-to-r border-[#e2e2e220] border-b'
+            className='relative h-[6rem] overflow-hidden bg-blue-gray-500 bg-clip-border text-white  bg-gradient-to-r border-[#e2e2e220] border-b'
           ></div>
           <div className='px-4 py-3 flex items-center justify-between'>
             <h5 className='block text-md font-sans font-semibold leading-snug tracking-normal text-white antialiased'>
