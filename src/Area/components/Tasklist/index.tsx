9203d255bb58ab2b562b219c954cb251643d27ac
mohmmedalkhatim <developmentaccun@outlook.com>
2025-10-01 09:38:18 +0200

    rebuilding the task list structure

--- src/Area/components/Tasklist/index.tsx
+++ src/Area/components/Tasklist/index.tsx
@@ -24,6 +24,7 @@ function TaskList({ id, cols }: { id: number, cols: number | undefined }) {
   const card = useAreas(state => state.get_Card)(id);
   const updateCard = useAreas(state => state.update_card);
   const act = useAreas(state => state.active);
+  let [_, setRender] = useState(0);
   const sensors = useSensors(
     useSensor(PointerSensor),
     useSensor(KeyboardSensor)
@@ -34,7 +35,7 @@ function TaskList({ id, cols }: { id: number, cols: number | undefined }) {
       updateCard(id, card)
     }
     setSchema(card.props.columns);
-  }, [cols]);
+  }, [act, cols]);
 
   const handleDragStart = (event: any) => {
     setActiveId(event.active.id);
@@ -42,53 +43,61 @@ function TaskList({ id, cols }: { id: number, cols: number | undefined }) {
 
   const handleDragEnd = async (event: any, column_id: number) => {
     if (event.active.id !== event.over.id) {
-      const oldIndex = schema?.indexOf(event.active.id);
-      const newIndex = schema?.indexOf(event.over.id);
-      const updated = arrayMove(schema, oldIndex, newIndex);
-      card.props.columns[column_id].list = updated;
-      setSchema(updated);
+      const oldIndex = schema[column_id].list?.indexOf(event.active.id);
+      const newIndex = schema[column_id].list?.indexOf(event.over.id);
+      let temp = [...schema[column_id].list];
+      const updated = arrayMove(temp, oldIndex, newIndex);
+      card.props.columns[column_id].list = [...updated];
+      setSchema(state => {
+        let temp = state;
+        temp[column_id].list = [...updated]
+        return temp;
+      });
       updateCard(card.id, card);
     }
     setActiveId(null);
   };
-  console.log(cols)
   if (schema) {
     return (
       <div className='flex w-full'>
-        {schema.map((column, id) => (
-          <DndContext
-            sensors={sensors}
-            collisionDetection={closestCenter}
-            onDragStart={handleDragStart}
-            onDragEnd={event => handleDragEnd(event, id)}
-          >
-            <SortableContext items={column.list} strategy={SwappingStrategy}>
-              <div className='p-2 w-full'>
-                <Form column_id={id} card_id={card.id} />
-                <div className='task_list overflow-hidden'>
-                  {column.list?.map(item =>
-                    item !== null ? (
-                      <Task
-                        id={item}
-                        key={item}
-                        classname={activeId === String(item) ? 'dragging' : ''}
-                      />
-                    ) : (
-                      ''
-                    )
-                  )}
-                </div>
-                <div
-                  className='absolute bottom-5 right-5'
-                  onClick={() => open_dialog('dialog_links', { id })}
-                >
-                  <IconLink size={'1.3rem'} color='gray' />
-                </div>
-              </div>
-            </SortableContext>
-            <DragOverlay></DragOverlay>
-          </DndContext>
-        ))}
+        {schema.map((column, id) => {
+          if (cols && cols > id) {
+            return (
+              <DndContext
+                sensors={sensors}
+                collisionDetection={closestCenter}
+                onDragStart={handleDragStart}
+                onDragEnd={event => handleDragEnd(event, id)}
+              >
+                <SortableContext items={column.list} strategy={SwappingStrategy}>
+                  <div className='p-2 w-full'>
+                    <Form column_id={id} card_id={card.id} setActive={setRender} />
+                    <div className='task_list overflow-hidden'>
+                      {column.list?.map(item =>
+                        item !== null ? (
+                          <Task
+                            id={item}
+                            key={item}
+                            classname={activeId === String(item) ? 'dragging' : ''}
+                          />
+                        ) : (
+                          ''
+                        )
+                      )}
+                    </div>
+                    <div
+                      className='absolute bottom-5 right-5'
+                      onClick={() => open_dialog('dialog_links', { id })}
+                    >
+                      <IconLink size={'1.3rem'} color='gray' />
+                    </div>
+                  </div>
+                  <DragOverlay></DragOverlay>
+                </SortableContext>
+              </DndContext>
+            )
+          }
+        })}
 
       </div>
     );
