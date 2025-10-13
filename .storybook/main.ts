ecfe39c1d36ea10bf6d66743cb3bb899fd66ba7d
mohmmedalkhatim <developmentaccun@outlook.com>
2025-10-13 13:23:16 +0200

    creating the view for the event menu

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,6 +1,7 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
+  "docs":{"docsMode":true},
   "stories": [
     "../src/**/*.mdx",
     "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
