4497a0d0c863a8de058591e87faf9e1d1925b281
mohmmedalkhatim <developmentaccun@outlook.com>
2025-11-06 20:05:20 +0200

    feat: implement Item component with styles and functionality for Kanban

---
+++ src/Area/components/kanban/dnd_kanban/components/Item/Item.tsx
@@ -0,0 +1,187 @@
+import React, { useEffect, useState } from 'react';
+import classNames from 'classnames';
+import type { DraggableSyntheticListeners } from '@dnd-kit/core';
+import type { Transform } from '@dnd-kit/utilities';
+
+import { Handle, Remove } from './components';
+
+import styles from './Item.module.css';
+import { useDebounce } from 'react-use';
+import { useLayoutDialog } from '../../../../../../context/para/Dialog';
+import { useTasks } from '../../../../../../context/tasks';
+import { Todo } from '../../../../../../types/todos';
+
+export interface Props {
+  dragOverlay?: boolean;
+  color?: string;
+  disabled?: boolean;
+  dragging?: boolean;
+  handle?: boolean;
+  handleProps?: any;
+  height?: number;
+  index?: number;
+  fadeIn?: boolean;
+  transform?: Transform | null;
+  listeners?: DraggableSyntheticListeners;
+  sorting?: boolean;
+  style?: React.CSSProperties;
+  transition?: string | null;
+  wrapperStyle?: React.CSSProperties;
+  value: React.ReactNode;
+  onRemove?(): void;
+  renderItem?(args: {
+    dragOverlay: boolean;
+    dragging: boolean;
+    sorting: boolean;
+    index: number | undefined;
+    fadeIn: boolean;
+    listeners: DraggableSyntheticListeners;
+    ref: React.Ref<HTMLElement>;
+    style: React.CSSProperties | undefined;
+    transform: Props['transform'];
+    transition: Props['transition'];
+    value: Props['value'];
+  }): React.ReactElement;
+}
+
+export const Item = React.memo(
+  React.forwardRef<HTMLLIElement, Props>(
+    (
+      {
+        color,
+        dragOverlay,
+        dragging,
+        disabled,
+        fadeIn,
+        handle,
+        handleProps,
+        height,
+        index,
+        listeners,
+        onRemove,
+        renderItem,
+        sorting,
+        style,
+        transition,
+        transform,
+        value,
+        wrapperStyle,
+        ...props
+      },
+      ref
+    ) => {
+       const one = useTasks(state => state.get_one);
+  const [task, setTask] = useState<Todo | undefined>();
+  const [checked, setChecked] = useState(task?.checked || false);
+  const dialog = useLayoutDialog(state => state.changeMode);
+  const update = useTasks(state => state.update);
+  useDebounce(() => {
+    if (!task) {
+      one(String(value), setTask, setChecked).then(() => { });
+    }
+    return task
+  }, 20, [])
+  useDebounce(
+    () => {
+      if (task) {
+        update(
+          {
+            id: task?.id,
+            title: task?.title,
+            checked,
+            note_id: task?.note_id,
+          },
+          setTask,
+          setChecked
+        );
+      }
+    },
+    10,
+    [checked]
+  );
+      useEffect(() => {
+        if (!dragOverlay) {
+          return;
+        }
+
+        document.body.style.cursor = 'grabbing';
+
+        return () => {
+          document.body.style.cursor = '';
+        };
+      }, [dragOverlay]);
+
+      return renderItem ? (
+        renderItem({
+          dragOverlay: Boolean(dragOverlay),
+          dragging: Boolean(dragging),
+          sorting: Boolean(sorting),
+          index,
+          fadeIn: Boolean(fadeIn),
+          listeners,
+          ref,
+          style,
+          transform,
+          transition,
+          value,
+        })
+      ) : (
+        <li
+          className={classNames(
+            styles.Wrapper,
+            fadeIn && styles.fadeIn,
+            sorting && styles.sorting,
+            dragOverlay && styles.dragOverlay,
+          )}
+          style={
+            {
+              ...wrapperStyle,
+              transition: [transition, wrapperStyle?.transition]
+                .filter(Boolean)
+                .join(', '),
+              '--translate-x': transform
+                ? `${Math.round(transform.x)}px`
+                : undefined,
+              '--translate-y': transform
+                ? `${Math.round(transform.y)}px`
+                : undefined,
+              '--scale-x': transform?.scaleX
+                ? `${transform.scaleX}`
+                : undefined,
+              '--scale-y': transform?.scaleY
+                ? `${transform.scaleY}`
+                : undefined,
+              '--index': index,
+              '--color': color,
+            } as React.CSSProperties
+          }
+          ref={ref}
+        >
+          <div
+            className={classNames(
+              styles.Item,
+              dragging && styles.dragging,
+              handle && styles.withHandle,
+              dragOverlay && styles.dragOverlay,
+              disabled && styles.disabled,
+              color && styles.color
+            )}
+            style={style}
+            data-cypress="draggable-item"
+            {...(!handle ? listeners : undefined)}
+            {...props}
+            tabIndex={!handle ? 0 : undefined}
+          >
+            {task?.title}
+            <span className={styles.Actions}>
+              {onRemove ? (
+                <Remove className={styles.Remove} onClick={onRemove} />
+              ) : null}
+              {handle ? <Handle {...handleProps} {...listeners} /> : null}
+            </span>
+          </div>
+        </li>
+      );
+    }
+  )
+);
