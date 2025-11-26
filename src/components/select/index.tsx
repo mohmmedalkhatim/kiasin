f85d24b83facbc481a22a6e849c30d5e5a0e14cb
mohmmedalkhatim <developmentaccun@outlook.com>
2025-11-26 09:50:26 +0200

    feat: create a component for the database column head

--- src/components/select/index.tsx
+++ src/components/select/index.tsx
@@ -1,19 +1,21 @@
 
 type SelectProps = React.DetailedHTMLProps<React.SelectHTMLAttributes<HTMLSelectElement>, HTMLSelectElement>
-type OptionProps  = React.DetailedHTMLProps<React.OptionHTMLAttributes<HTMLOptionElement>, HTMLOptionElement>;
+type OptionProps = React.DetailedHTMLProps<React.OptionHTMLAttributes<HTMLOptionElement>, HTMLOptionElement>;
 
 
-function Select(props:SelectProps) {
+function Select(props: SelectProps) {
   return (
-    <select {...props}></select>
+    <div className="p-1 rounded w-full m_border pr-2">
+      <select {...props} className="pl-0  focus:outline-none px-3 text-xs text-[#e2e2e2e2] w-full  bg-[#181818] rounded"></select>
+    </div>
   )
 }
-Select.Option = (props:OptionProps)=>{
-    return (
-        <option {...props}>
-            
-        </option>
-    )
+Select.Option = (props: OptionProps) => {
+  return (
+    <option {...props}>
+
+    </option>
+  )
 }
 
 export default Select
